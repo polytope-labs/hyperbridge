4d4fd9ddb5c3aeca91af5e133b73c25453dd8358
Seun Lanlege <seunlanlege@gmail.com>
2023-01-20 19:19:29 +0100

    add workflow

--- src/node_codec.rs
+++ src/node_codec.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with OpenEthereum.  If not, see <http://www.gnu.org/licenses/>.
 
-//! `NodeCodec` implementation for Rlp
+//! [`NodeCodec`] implementation for [`TrieDb`]
 
 use alloc::vec::Vec;
 use core::{borrow::Borrow, marker::PhantomData};

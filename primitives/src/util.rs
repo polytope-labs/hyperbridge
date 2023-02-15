63a9c7a09ad06e525677424571b6bf2ead0c7161
Seun Lanlege <seunlanlege@gmail.com>
2023-02-15 19:21:46 -0400

    clean up code

--- primitives/src/util.rs
+++ primitives/src/util.rs
@@ -1,46 +1,45 @@
 use base2::Base2;
-use ethereum_consensus::altair::mainnet::EPOCHS_PER_SYNC_COMMITTEE_PERIOD;
-use ethereum_consensus::configs::mainnet::{
-    ALTAIR_FORK_EPOCH, ALTAIR_FORK_VERSION, GENESIS_FORK_VERSION,
+use ethereum_consensus::{
+	altair::mainnet::EPOCHS_PER_SYNC_COMMITTEE_PERIOD,
+	configs::mainnet::{ALTAIR_FORK_EPOCH, ALTAIR_FORK_VERSION, GENESIS_FORK_VERSION},
+	phase0::mainnet::SLOTS_PER_EPOCH,
 };
-use ethereum_consensus::phase0::mainnet::SLOTS_PER_EPOCH;
-use ethereum_consensus::primitives::{Hash32, Root};
-use ssz_rs::{MerkleizationError, Node};
+use ssz_rs::Node;
 
 /// Calculate the subtree index from the ``generalized_index``
 pub fn get_subtree_index(generalized_index: u64) -> u64 {
-    generalized_index % 2 ^ (generalized_index.floor_log2() as u64)
+	generalized_index % 2 ^ (generalized_index.floor_log2() as u64)
 }
 
 /// Return the sync committe period at the given ``epoch``
 pub fn compute_sync_committee_period(epoch: u64) -> u64 {
-    epoch / EPOCHS_PER_SYNC_COMMITTEE_PERIOD
+	epoch / EPOCHS_PER_SYNC_COMMITTEE_PERIOD
 }
 
 /// Return the epoch number at ``slot``.
 pub fn compute_epoch_at_slot(slot: u64) -> u64 {
-    slot / SLOTS_PER_EPOCH
+	slot / SLOTS_PER_EPOCH
 }
 
 /// Return the fork version at the given ``epoch``.
 pub fn compute_fork_version(epoch: u64) -> [u8; 4] {
-    if epoch >= ALTAIR_FORK_EPOCH {
-        ALTAIR_FORK_VERSION
-    } else {
-        GENESIS_FORK_VERSION
-    }
+	if epoch >= ALTAIR_FORK_EPOCH {
+		ALTAIR_FORK_VERSION
+	} else {
+		GENESIS_FORK_VERSION
+	}
 }
 
 /// Return the sync committee period at ``slot``
 pub fn compute_sync_committee_period_at_slot(slot: u64) -> u64 {
-    compute_sync_committee_period(compute_epoch_at_slot(slot))
+	compute_sync_committee_period(compute_epoch_at_slot(slot))
 }
 
 /// method for hashing objects into a single root by utilizing a hash tree structure, as defined in
 /// the SSZ spec.
 pub fn hash_tree_root<T: ssz_rs::SimpleSerialize>(
-    mut object: T,
+	mut object: T,
 ) -> Result<Node, ssz_rs::MerkleizationError> {
-    let root = object.hash_tree_root()?;
-    Ok(root)
+	let root = object.hash_tree_root()?;
+	Ok(root)
 }
--- prover/src/responses/finality_checkpoint_response.rs
+++ prover/src/responses/finality_checkpoint_response.rs
@@ -2,13 +2,13 @@ use ethereum_consensus::bellatrix::Checkpoint;
 
 #[derive(Default, Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 pub(crate) struct Response {
-    execution_optimistic: bool,
-    pub data: ResponseData,
+	execution_optimistic: bool,
+	pub data: ResponseData,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 pub struct ResponseData {
-    previous_justified: Checkpoint,
-    current_justified: Checkpoint,
-    pub finalized: Checkpoint,
+	previous_justified: Checkpoint,
+	current_justified: Checkpoint,
+	pub finalized: Checkpoint,
 }

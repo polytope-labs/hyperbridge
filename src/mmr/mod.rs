0cae867f1a9f564f524584e99565f51a8dfc8268
David Salami <wizdave97@gmail.com>
2023-03-17 07:36:08 +0100

    update licensing

--- src/mmr/mod.rs
+++ src/mmr/mod.rs
@@ -1,6 +1,4 @@
-// This file is part of Substrate.
-
-// Copyright (C) 2020-2022 Parity Technologies (UK) Ltd.
+// Copyright (C) 2023 Polytope Labs.
 // SPDX-License-Identifier: Apache-2.0
 
 // Licensed under the Apache License, Version 2.0 (the "License");

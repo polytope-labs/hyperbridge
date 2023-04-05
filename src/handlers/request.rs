dea54ad5e22ec4d35d0f076fe83a7c9afcd48052
Web3 Philosopher <seun@polytope.technology>
2023-04-05 18:23:43 +0100

    introduce Request::Get (#18)

---
+++ src/handlers/request.rs
@@ -0,0 +1,49 @@
+// Copyright (C) Polytope Labs Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! The ISMP request handler
+
+use crate::{
+    error::Error,
+    handlers::{validate_state_machine, MessageResult, RequestResponseResult},
+    host::ISMPHost,
+    messaging::RequestMessage,
+    router::RequestResponse,
+};
+
+/// Validate the state machine, verify the request message and dispatch the message to the router
+pub fn handle(host: &dyn ISMPHost, msg: RequestMessage) -> Result<MessageResult, Error> {
+    let consensus_client = validate_state_machine(host, &msg.proof)?;
+    // Verify membership proof
+    let state = host.state_machine_commitment(msg.proof.height)?;
+    consensus_client.verify_membership(
+        host,
+        RequestResponse::Request(msg.request.clone()),
+        state.ismp_root,
+        &msg.proof,
+    )?;
+
+    let router = host.ismp_router();
+
+    let result = RequestResponseResult {
+        dest_chain: msg.request.dest_chain(),
+        source_chain: msg.request.source_chain(),
+        nonce: msg.request.nonce(),
+    };
+
+    router.dispatch(msg.request)?;
+
+    Ok(MessageResult::Request(result))
+}
--- src/router.rs
+++ src/router.rs
@@ -1,31 +1,137 @@
-use crate::{error::Error, host::ChainID, prelude::Vec};
+// Copyright (C) Polytope Labs Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! ISMPRouter definition
+
+use crate::{consensus_client::StateMachineHeight, error::Error, host::ChainID, prelude::Vec};
 use codec::{Decode, Encode};
 
+/// The ISMP POST request.
 #[derive(Debug, Clone, Encode, Decode, PartialEq, Eq, scale_info::TypeInfo)]
 #[cfg_attr(feature = "std", derive(serde::Deserialize, serde::Serialize))]
-pub struct Request {
-    pub nonce: u64,
+pub struct POST {
+    /// The source state machine of this request.
     pub source_chain: ChainID,
+    /// The destination state machine of this request.
     pub dest_chain: ChainID,
+    /// The nonce of this request on the source chain
+    pub nonce: u64,
+    /// Moudle Id of the sending module
     pub from: Vec<u8>,
+    /// Module ID of the receiving module
     pub to: Vec<u8>,
+    /// Timestamp which this request expires by.
     pub timeout_timestamp: u64,
+    /// Encoded Request.
     pub data: Vec<u8>,
 }
 
+/// The ISMP GET request.
+#[derive(Debug, Clone, Encode, Decode, PartialEq, Eq, scale_info::TypeInfo)]
+#[cfg_attr(feature = "std", derive(serde::Deserialize, serde::Serialize))]
+pub struct GET {
+    /// The source state machine of this request.
+    pub source_chain: ChainID,
+    /// The destination state machine of this request.
+    pub dest_chain: ChainID,
+    /// The nonce of this request on the source chain
+    pub nonce: u64,
+    /// Moudle Id of the sending module
+    pub from: Vec<u8>,
+    /// Storage keys that this request is interested in.
+    pub keys: Vec<Vec<u8>>,
+    /// Height at which to read the state machine.
+    pub height: StateMachineHeight,
+    /// Timestamp which this request expires by.
+    pub timeout_timestamp: u64,
+}
+
+/// The ISMP request.
+#[derive(Debug, Clone, Encode, Decode, PartialEq, Eq, scale_info::TypeInfo)]
+#[cfg_attr(feature = "std", derive(serde::Deserialize, serde::Serialize))]
+pub enum Request {
+    /// A post request allows a module on a state machine to send arbitrary bytes to another module
+    /// living in another state machine.
+    Post(POST),
+    /// A get request allows a module on a state machine to read the storage of another module
+    /// living in another state machine.
+    Get(GET),
+}
+
+impl Request {
+    /// Get the source chain
+    pub fn source_chain(&self) -> ChainID {
+        match self {
+            Request::Get(get) => get.source_chain,
+            Request::Post(post) => post.source_chain,
+        }
+    }
+
+    /// Get the destination chain
+    pub fn dest_chain(&self) -> ChainID {
+        match self {
+            Request::Get(get) => get.dest_chain,
+            Request::Post(post) => post.dest_chain,
+        }
+    }
+
+    /// Get the request nonce
+    pub fn nonce(&self) -> u64 {
+        match self {
+            Request::Get(get) => get.nonce,
+            Request::Post(post) => post.nonce,
+        }
+    }
+
+    /// Get the POST request data
+    pub fn data(&self) -> Option<Vec<u8>> {
+        match self {
+            Request::Get(_) => None,
+            Request::Post(post) => Some(post.data.clone()),
+        }
+    }
+
+    /// Get the GET request keys.
+    pub fn keys(&self) -> Option<Vec<Vec<u8>>> {
+        match self {
+            Request::Post(_) => None,
+            Request::Get(get) => Some(get.keys.clone()),
+        }
+    }
+}
+
+/// The ISMP response
 #[derive(Debug, Clone, Encode, Decode, PartialEq, Eq, scale_info::TypeInfo)]
 #[cfg_attr(feature = "std", derive(serde::Deserialize, serde::Serialize))]
 pub struct Response {
+    /// The request that triggered this response.
     pub request: Request,
+    /// The response message.
     pub response: Vec<u8>,
 }
 
+/// This is the concrete type for Get requests
+pub type GetResponse = Vec<(Vec<u8>, Vec<u8>)>;
+
+/// Convenience enum for membership verification.
 pub enum RequestResponse {
     Request(Request),
     Response(Response),
 }
 
-pub trait IISMPRouter {
+pub trait ISMPRouter {
     /// Dispatch a request from a module to the ISMP router.
     /// If request source chain is the host, it should be committed in state as a sha256 hash
     fn dispatch(&self, request: Request) -> Result<(), Error>;

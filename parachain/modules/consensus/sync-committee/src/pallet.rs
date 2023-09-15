218070fe5ad6ed7f18593028b64cee3a34d1a390
David Salami <wizdave97@gmail.com>
2023-09-15 12:35:11 +0100

    add extra call to sync committee pallet

--- parachain/modules/consensus/sync-committee/src/pallet.rs
+++ parachain/modules/consensus/sync-committee/src/pallet.rs
@@ -68,5 +68,37 @@ pub mod pallet {
                 .map_err(|_| Error::<T>::ErrorStoringConsensusState)?;
             Ok(())
         }
+
+        /// Update l2 oracle or rollup core address
+        #[pallet::call_index(1)]
+        #[pallet::weight(<T as frame_system::Config>::DbWeight::get().reads_writes(1, 1))]
+        pub fn update_l2_addresses(
+            origin: OriginFor<T>,
+            l2_oracle: Option<H160>,
+            rollup_core: Option<H160>,
+        ) -> DispatchResult {
+            <T as Config>::AdminOrigin::ensure_origin(origin)?;
+
+            let ismp_host = Host::<T>::default();
+
+            let encoded_consensus_state = ismp_host
+                .consensus_state(BEACON_CONSENSUS_STATE_ID)
+                .map_err(|_| Error::<T>::ErrorFetchingConsensusState)?;
+            let mut consensus_state: ConsensusState =
+                codec::Decode::decode(&mut &encoded_consensus_state[..])
+                    .map_err(|_| Error::<T>::ErrorDecodingConsensusState)?;
+            if let Some(l2_oracle) = l2_oracle {
+                consensus_state.l2_oracle_address = l2_oracle;
+            }
+            if let Some(rollup_core) = rollup_core {
+                consensus_state.rollup_core_address = rollup_core;
+            }
+
+            let encoded_consensus_state = consensus_state.encode();
+            ismp_host
+                .store_consensus_state(BEACON_CONSENSUS_STATE_ID, encoded_consensus_state)
+                .map_err(|_| Error::<T>::ErrorStoringConsensusState)?;
+            Ok(())
+        }
     }
 }

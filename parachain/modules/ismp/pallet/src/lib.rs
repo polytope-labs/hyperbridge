11915429fcb8b0aa5b1fcfa3ad8d43afec35106d
David Salami <31099392+Wizdave97@users.noreply.github.com>
2023-10-19 21:42:31 +0100

    Fix getter function naming (#50)

--- parachain/modules/ismp/pallet/src/lib.rs
+++ parachain/modules/ismp/pallet/src/lib.rs
@@ -170,15 +170,15 @@ pub mod pallet {
     /// Holds a map of state machines to the height at which they've been frozen due to byzantine
     /// behaviour
     #[pallet::storage]
-    #[pallet::getter(fn latest_messaging_heights)]
+    #[pallet::getter(fn frozen_heights)]
     pub type FrozenHeights<T: Config> =
         StorageMap<_, Blake2_128Concat, StateMachineId, u64, OptionQuery>;
 
     /// Holds a map of state machines to the latest height we've processed requests for
     #[pallet::storage]
-    #[pallet::getter(fn frozen_heights)]
+    #[pallet::getter(fn latest_messaging_heights)]
     pub type LatestMessagingHeight<T: Config> =
-        StorageMap<_, Blake2_128Concat, StateMachineId, u64, ValueQuery>;
+        StorageMap<_, Blake2_128Concat, StateMachineId, u64, OptionQuery>;
 
     /// A mapping of ConsensusStateId to ConsensusClientId
     #[pallet::storage]
@@ -519,7 +519,7 @@ impl<T: Config> Pallet<T> {
                         _ => unreachable!(),
                     };
                     // update the messaging heights
-                    if LatestMessagingHeight::<T>::get(&id) < height {
+                    if LatestMessagingHeight::<T>::get(&id) < Some(height) {
                         LatestMessagingHeight::<T>::insert(id, height);
                     }
                     debug!(target: "ismp-modules", "Module Callback Results {:?}", ModuleCallbackResult::Response(res));
@@ -530,7 +530,7 @@ impl<T: Config> Pallet<T> {
                         _ => unreachable!(),
                     };
                     // update the messaging heights
-                    if LatestMessagingHeight::<T>::get(&id) < height {
+                    if LatestMessagingHeight::<T>::get(&id) < Some(height) {
                         LatestMessagingHeight::<T>::insert(id, height);
                     }
                     debug!(target: "ismp-modules", "Module Callback Results {:?}", ModuleCallbackResult::Request(res));

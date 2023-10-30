a9d008352f845a18a229e240d0f92a91bdba91b1
David Salami <wizdave97@gmail.com>
2023-10-30 14:56:23 +0100

    add cross chain token transfer

--- messenger/src/main.rs
+++ messenger/src/main.rs
@@ -47,9 +47,16 @@ use subxt::{
     utils::{AccountId32, MultiAddress, MultiSignature, H256},
     OnlineClient,
 };
-use types::runtime::api::runtime_types::ismp::consensus::StateMachineId;
+use types::{
+    runtime::api::runtime_types::ismp::consensus::StateMachineId,
+    token_faucet::TokenFaucet,
+    token_gateway::{AssetReceivedFilter, SendParams, TokenGateway},
+};
 
 static CROSS_CHAIN_MESSENGER_ADDRESS: H160 = H160(hex!("7067AC432584D7a00A75804EaF010498c263819d"));
+static GATEWAY_ADDRESS: H160 = H160(hex!("0e366Fa0506F2dfF080648a99083A6a2F7D175e0"));
+static FAUCET_ADDRESS: H160 = H160(hex!("368F3d726C904C0113b6C8A8923D4Bfe8b0b19E7"));
+static MULTICHAIN_TOKEN: H160 = H160(hex!("00155be25de526F458D1C303feEA6983Dbe007ad"));
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
@@ -59,7 +66,6 @@ async fn main() -> Result<(), anyhow::Error> {
         .init();
     let args = Cli::parse();
     let source: Ethereum = args.source.into();
-    let destination: Ethereum = args.destination.into();
 
     // initialize clients
     let bytes = hex::decode(args.signer.as_str())?;
@@ -67,45 +73,105 @@ async fn main() -> Result<(), anyhow::Error> {
     let signer = LocalWallet::from(SecretKey::from_slice(signer.seed().as_slice())?)
         .with_chain_id(source.chain_id());
     let provider = Provider::<Ws>::connect_with_reconnects(source.execution_rpc(), 1000).await?;
-    let substrate =
+    let mut substrate =
         OnlineClient::<KeccakSubstrateChain>::from_url("ws://34.22.152.185:9933").await?;
-    let rpc_client = WsClientBuilder::default().build(&destination.execution_rpc()).await?;
     let signer = Arc::new(provider.clone().with_signer(signer));
 
     // initiate transaction
     let pb = progress_bar(format!("Sending transaction .."));
     let now = Instant::now();
-    let messenger = CrossChainMessenger::new(CROSS_CHAIN_MESSENGER_ADDRESS, signer.clone());
-    let receipt = messenger
-        .teleport(CrossChainMessage {
-            dest: StateMachine::Ethereum(destination).to_string().as_bytes().to_vec().into(),
-            message: args.message.as_bytes().to_vec().into(),
-            timeout: 3 * 60 * 60,
-        })
-        .gas(100_000)
-        .send()
-        .await?
-        .await?
-        .ok_or_else(|| anyhow!("transaction failed i guess"))?;
+    let (receipt, destination) = match args.action.clone() {
+        Action::Gateway { to, amount, destination } => {
+            let destination: Ethereum = destination.into();
+            let gateway = TokenGateway::new(GATEWAY_ADDRESS, signer.clone());
+            let receipt = gateway
+                    .send(SendParams {
+                        amount: (amount * 10u128.pow(18)).into(),
+                        to: {
+                            let to = to
+                                .split("0x")
+                                .last()
+                                .ok_or_else(|| anyhow!("Invalid destination adress"))?;
+                            let bytes = hex::decode(to)?;
+                            if bytes.len() != 20 {
+                                Err(anyhow!("Invalid destination adress"))?
+                            }
+                            H160::from_slice(&bytes)
+                        },
+                        dest: StateMachine::Ethereum(destination)
+                            .to_string()
+                            .as_bytes()
+                            .to_vec()
+                            .into(),
+                        token_contract: MULTICHAIN_TOKEN,
+                        timeout: 3 * 60 * 60,
+                    })
+                    .gas(100_000)
+                    .send()
+                    .await?
+                    .await?
+                    .ok_or_else(|| anyhow!("Transaction failed, please ensure you have some balance in your account, if not use the `drip` command first"))?;
+            (receipt, destination)
+        },
+        Action::Drip => {
+            let faucet = TokenFaucet::new(FAUCET_ADDRESS, signer.clone());
+            let receipt = faucet
+                .drip()
+                .gas(100_000)
+                .send()
+                .await?
+                .await?
+                .ok_or_else(|| anyhow!("Transaction submission failed"))?;
+            pb.finish_with_message(format!(
+                "Drip completed: {}, took: {}",
+                source.etherscan(receipt.transaction_hash),
+                HumanDuration(now.elapsed())
+            ));
+            return Ok(())
+        },
+        Action::Ping { destination, message } => {
+            let destination: Ethereum = destination.into();
+            let messenger = CrossChainMessenger::new(CROSS_CHAIN_MESSENGER_ADDRESS, signer.clone());
+            let receipt = messenger
+                .teleport(CrossChainMessage {
+                    dest: StateMachine::Ethereum(destination)
+                        .to_string()
+                        .as_bytes()
+                        .to_vec()
+                        .into(),
+                    message: message.as_bytes().to_vec().into(),
+                    timeout: 3 * 60 * 60,
+                })
+                .gas(100_000)
+                .send()
+                .await?
+                .await?
+                .ok_or_else(|| anyhow!("transaction failed i guess"))?;
+            (receipt, destination)
+        },
+    };
+
+    let mut rpc_client = WsClientBuilder::default().build(&destination.execution_rpc()).await?;
+
     let block_number = receipt.block_number.unwrap().as_u64();
+
     pb.finish_with_message(format!(
         "Cross chain message sent: {}, took: {}",
         source.etherscan(receipt.transaction_hash),
         HumanDuration(now.elapsed())
     ));
-    let request = parse_log::<PostRequestEventFilter>(receipt.logs[0].clone())?;
+    let request = receipt
+        .logs
+        .iter()
+        .filter_map(|log| parse_log::<PostRequestEventFilter>(log.clone()).ok())
+        .collect::<Vec<_>>()
+        .get(0)
+        .cloned()
+        .ok_or_else(|| anyhow!("Post Request was not sent, Ensure you have some balance by using the `drip` command before performing a token transfer"))?;
 
     // wait for Ethereum finality
     let now = Instant::now();
-    let subscription = substrate
-        .rpc()
-        .subscribe::<StorageChangeSet<H256>>(
-            "state_subscribeStorage",
-            rpc_params![vec![system_events_key()]],
-            "state_unsubscribeStorage",
-        )
-        .await
-        .expect("Storage subscription failed");
+    let subscription = subscribe_storage(&mut substrate).await;
     let mut debounced_sub = Debounced::new(subscription, Duration::from_secs(4));
     let pb = progress_bar("Waiting for Ethereum to finalize your transaction".into());
     'outer: loop {
@@ -244,7 +310,7 @@ async fn main() -> Result<(), anyhow::Error> {
 
     let mut stream = {
         let stream =
-            subscribe_logs(&rpc_client, destination.handler())
+            subscribe_logs(&mut rpc_client, &destination.execution_rpc(), destination.handler())
                 .await
                 .filter_map(|log| async move {
                     log.ok().and_then(|log| {
@@ -280,39 +346,82 @@ async fn main() -> Result<(), anyhow::Error> {
     // wait for hyperbridge delivery
     let now = Instant::now();
     let pb = progress_bar("Waiting for Hyperbridge to deliver your transaction".into());
-    let mut stream = {
-        let stream = subscribe_logs(&rpc_client, CROSS_CHAIN_MESSENGER_ADDRESS).await.filter_map(
-            |log| async move {
-                log.ok().and_then(|log| {
-                    parse_log::<PostReceivedFilter>(log.clone())
-                        .map(|ev| (log.transaction_hash, ev))
-                        .ok()
-                })
-            },
-        );
-        Box::pin(stream)
-    };
+    match args.action {
+        Action::Gateway { .. } => {
+            let mut stream = {
+                let stream =
+                    subscribe_logs(&mut rpc_client, &destination.execution_rpc(), GATEWAY_ADDRESS)
+                        .await
+                        .filter_map(|log| async move {
+                            log.ok().and_then(|log| {
+                                parse_log::<AssetReceivedFilter>(log.clone())
+                                    .map(|ev| (log.transaction_hash, ev))
+                                    .ok()
+                            })
+                        });
+                Box::pin(stream)
+            };
 
-    loop {
-        let (hash, event) = match stream.next().await {
-            Some(message) => message,
-            None => {
-                panic!("Hyperbridge delivery stream terminated unexpectedly");
-            },
-        };
+            loop {
+                let (hash, event) = match stream.next().await {
+                    Some(message) => message,
+                    None => {
+                        panic!("Hyperbridge delivery stream terminated unexpectedly");
+                    },
+                };
 
-        if args.message == event.message &&
-            event.nonce == request.nonce &&
-            request.source == event.source
-        {
-            pb.finish_with_message(format!(
-                "Hyperbridge has delivered your transaction: {}, took: {}",
-                destination.etherscan(hash.unwrap()),
-                HumanDuration(now.elapsed())
-            ));
+                if event.nonce == request.nonce && request.source == event.source {
+                    pb.finish_with_message(format!(
+                        "Hyperbridge has delivered your transaction, check your wallet balance: {}, took: {}",
+                        destination.etherscan(hash.unwrap()),
+                        HumanDuration(now.elapsed())
+                    ));
 
-            break
-        }
+                    break
+                }
+            }
+        },
+        Action::Ping { message, .. } => {
+            let mut stream = {
+                let stream = subscribe_logs(
+                    &mut rpc_client,
+                    &destination.execution_rpc(),
+                    CROSS_CHAIN_MESSENGER_ADDRESS,
+                )
+                .await
+                .filter_map(|log| async move {
+                    log.ok().and_then(|log| {
+                        parse_log::<PostReceivedFilter>(log.clone())
+                            .map(|ev| (log.transaction_hash, ev))
+                            .ok()
+                    })
+                });
+                Box::pin(stream)
+            };
+
+            loop {
+                let (hash, event) = match stream.next().await {
+                    Some(message) => message,
+                    None => {
+                        panic!("Hyperbridge delivery stream terminated unexpectedly");
+                    },
+                };
+
+                if message == event.message &&
+                    event.nonce == request.nonce &&
+                    request.source == event.source
+                {
+                    pb.finish_with_message(format!(
+                        "Hyperbridge has delivered your transaction: {}, took: {}",
+                        destination.etherscan(hash.unwrap()),
+                        HumanDuration(now.elapsed())
+                    ));
+
+                    break
+                }
+            }
+        },
+        _ => unreachable!(),
     }
 
     Ok(())
@@ -346,7 +455,8 @@ fn progress_bar(msg: String) -> ProgressBar {
 }
 
 async fn subscribe_logs(
-    rpc_client: &WsClient,
+    rpc_client: &mut WsClient,
+    rpc_addr: &str,
     address: H160,
 ) -> jsonrpsee::core::client::Subscription<Log> {
     let mut obj = ObjectParams::new();
@@ -354,14 +464,54 @@ async fn subscribe_logs(
     obj.insert("address", address.as_str())
         .expect("handler address should be valid");
     let param = obj.to_rpc_params().ok().flatten().expect("Failed to serialize rpc params");
-    rpc_client
-        .subscribe::<Log, _>(
-            "eth_subscribe",
-            jsonrpsee::rpc_params!("logs", param),
-            "eth_unsubscribe",
-        )
-        .await
-        .expect("Failed to susbcribe")
+    let reconnects = 10;
+    for _ in 0..reconnects {
+        let res = rpc_client
+            .subscribe::<Log, _>(
+                "eth_subscribe",
+                jsonrpsee::rpc_params!("logs", param.clone()),
+                "eth_unsubscribe",
+            )
+            .await;
+        match res {
+            Ok(sub) => return sub,
+            Err(_) => {
+                tokio::time::sleep(Duration::from_secs(5)).await;
+                if let Ok(client) = WsClientBuilder::default().build(&rpc_addr).await {
+                    *rpc_client = client
+                }
+            },
+        }
+    }
+    panic!("Rpc provider is down, Your message will still be delivered by the relayer, check your wallet balance in about 30mins")
+}
+
+async fn subscribe_storage(
+    substrate: &mut OnlineClient<KeccakSubstrateChain>,
+) -> subxt::rpc::Subscription<StorageChangeSet<H256>> {
+    let reconnects = 10;
+    for _ in 0..reconnects {
+        let err = substrate
+            .rpc()
+            .subscribe::<StorageChangeSet<H256>>(
+                "state_subscribeStorage",
+                rpc_params![vec![system_events_key()]],
+                "state_unsubscribeStorage",
+            )
+            .await;
+        match err {
+            Ok(sub) => return sub,
+            Err(_) => {
+                tokio::time::sleep(Duration::from_secs(5)).await;
+                if let Ok(client) =
+                    OnlineClient::<KeccakSubstrateChain>::from_url("ws://34.22.152.185:9933").await
+                {
+                    *substrate = client
+                }
+            },
+        }
+    }
+    panic!("Rpc provider is down, Your message will still be delivered by the relayer")
 }
 
 /// Some metadata about each chain
@@ -411,8 +561,9 @@ impl ChainInfo for Ethereum {
         match self {
             Ethereum::Base => format!("https://goerli.basescan.org/tx/{transaction:?}"),
             Ethereum::ExecutionLayer => format!("https://goerli.etherscan.io/tx/{transaction:?}"),
-            Ethereum::Optimism =>
-                format!("https://goerli-optimism.etherscan.io/tx/{transaction:?}"),
+            Ethereum::Optimism => {
+                format!("https://goerli-optimism.etherscan.io/tx/{transaction:?}")
+            },
             Ethereum::Arbitrum => format!("https://testnet.arbiscan.io/tx/{transaction:?}"),
         }
     }
@@ -446,21 +597,46 @@ impl subxt::Config for KeccakSubstrateChain {
 /// A simple CLI application for sending arbitrary messages through Hyperbridge
 #[derive(Parser, Debug)]
 pub struct Cli {
+    /// Sub commands for the messenger
+    #[command(subcommand)]
+    action: Action,
+
     /// Raw account secret key
     #[arg(short, long)]
     signer: String,
 
-    /// The source chain for the cross-chain message
+    /// The source chain for the transaction
     #[arg(value_enum, short, long)]
     source: Network,
+}
 
-    /// The destination chain for the cross-chain message
-    #[arg(value_enum, short, long)]
-    destination: Network,
+#[derive(Debug, clap::Subcommand, Clone)]
+pub enum Action {
+    /// Transfer tokens through the token gateway,
+    /// Ensure you have some balance by running the `drip` command first
+    Gateway {
+        /// Account to receive this transfer on the destination chain
+        #[arg(short, long)]
+        to: String,
+        /// Amount to be transferred
+        #[arg(short, long)]
+        amount: u128,
+        /// The destination chain for the token transfer
+        #[arg(value_enum, short, long)]
+        destination: Network,
+    },
+    /// Send an arbitrary cross chain message
+    Ping {
+        /// The destination chain for the cross-chain message
+        #[arg(value_enum, short, long)]
+        destination: Network,
 
-    /// Cross-chain message to be sent.
-    #[arg(short, long)]
-    message: String,
+        /// Cross-chain message to be sent.
+        #[arg(short, long)]
+        message: String,
+    },
+    /// Get daily drip from the token faucet
+    Drip,
 }
 
 #[derive(clap::ValueEnum, Debug, Clone, Copy)]
--- messenger/src/types.rs
+++ messenger/src/types.rs
@@ -3,3 +3,5 @@ pub mod handler;
 pub mod ismp_host;
 pub mod runtime;
 pub mod shared_types;
+pub mod token_faucet;
+pub mod token_gateway;

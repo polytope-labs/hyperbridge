168345f8c285f5a90b1cbe5cc256eb1345dbc1a0
Michael Amadi <amadimichaeld@gmail.com>
2024-01-29 12:08:21 +0100

    use erc6160

--- evm/src/GovernableToken.sol
+++ evm/src/GovernableToken.sol
@@ -3,38 +3,32 @@ pragma solidity 0.8.17;
 
 import {ERC20} from "openzeppelin/token/ERC20/extensions/ERC20Permit.sol";
 import "ismp/IIsmpModule.sol";
-import "multi-chain-tokens/interfaces/IERC6160Ext20.sol";
+import "../lib/ERC6160/src/tokens/ERC6160Ext20.sol";
 
-contract GovernableToken is ERC20, IIsmpModule, IERC6160Ext20 {
+contract GovernableToken is IIsmpModule, ERC6160Ext20 {
     bytes32 constant DEFAULT_ADMIN_ROLE = 0x0;
-    bytes32 constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 constant BURNER_ROLE = keccak256("BURNER_ROLE");
     bytes32 constant HOST_ROLE = keccak256("HOST_ROLE");
 
-    mapping(bytes32 => mapping(address => bool)) public hasRole;
-
-    constructor(string memory _name, string memory _symbol, address _defaultOwner) ERC20(_name, _symbol) {
-        _grantRole(DEFAULT_ADMIN_ROLE, _defaultOwner);
-    }
+    constructor(string memory _name, string memory _symbol, address _defaultOwner) ERC6160Ext20(_defaultOwner, _name, _symbol) {}
 
     modifier onlyRole(bytes32 role) {
-        require(hasRole[role][msg.sender], "Does not have role"); 
+        require(_roles[role][msg.sender], "Does not have role"); 
         _;
     }
 
     function _grantRole(bytes32 role, address account) private onlyRole(DEFAULT_ADMIN_ROLE) {
-        hasRole[role][account] = true;
+        _roles[role][account] = true;
     }
 
     function _revokeRole(bytes32 role, address account) private onlyRole(DEFAULT_ADMIN_ROLE) {
-        hasRole[role][account] = false;
+        _roles[role][account] = false;
     }
 
-    function mint(address account, uint256 amount, bytes calldata ) external onlyRole(MINTER_ROLE) {
+    function mint(address account, uint256 amount, bytes calldata) external override onlyRole(MINTER_ROLE) {
         _mint(account, amount);
     }
 
-    function burn(address account, uint256 amount, bytes calldata) external onlyRole(BURNER_ROLE)  {
+    function burn(address account, uint256 amount, bytes calldata) external override onlyRole(BURNER_ROLE)  {
         _burn(account, amount);
     }
 
@@ -61,15 +55,5 @@ contract GovernableToken is ERC20, IIsmpModule, IERC6160Ext20 {
 
     function onGetTimeout(GetRequest memory) external view onlyRole(HOST_ROLE) {
         revert("Token gateway doesn't emit Get requests");
-    }
-
-
-
-    function grantRole(bytes32, address) external pure {
-        revert();
-
-    }
-    function revokeRole(bytes32, address) external pure{
-        revert();
     }
 }

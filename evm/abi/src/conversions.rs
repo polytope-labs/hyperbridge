e3cf6b9e77b16f9594797cf2d5e033f016da2e47
Seun Lanlege <seun@polytope.technology>
2024-02-07 13:38:33 +0000

    add abi conversions for events

--- evm/abi/src/conversions.rs
+++ evm/abi/src/conversions.rs
@@ -21,6 +21,8 @@ use crate::{
         IntermediateState, Node, Parachain, ParachainProof, Payload, RelayChainProof,
         SignedCommitment, Vote,
     },
+    evm_host::EvmHostEvents,
+    handler::StateMachineUpdatedFilter,
     shared_types::{PostRequest, PostResponse, StateMachineHeight},
 };
 use anyhow::anyhow;
@@ -30,6 +32,7 @@ use beefy_verifier_primitives::{
 use ismp::{host::StateMachine, router};
 use merkle_mountain_range::{leaf_index_to_mmr_size, leaf_index_to_pos, mmr_position_to_k_index};
 use primitive_types::H256;
+use std::str::FromStr;
 
 impl From<beefy_verifier_primitives::ParachainProof> for ParachainProof {
     fn from(value: beefy_verifier_primitives::ParachainProof) -> Self {
@@ -217,6 +220,71 @@ impl TryFrom<ismp::consensus::StateMachineHeight> for StateMachineHeight {
             },
             height: value.height.into(),
         })
+    }
+}
+
+impl From<StateMachineUpdatedFilter> for ismp::events::StateMachineUpdated {
+    fn from(value: StateMachineUpdatedFilter) -> Self {
+        ismp::events::StateMachineUpdated {
+            state_machine_id: ismp::consensus::StateMachineId {
+                state_id: StateMachine::Kusama(value.state_machine_id.low_u64() as u32),
+                consensus_state_id: Default::default(),
+            },
+            latest_height: value.height.low_u64(),
+        }
+    }
+}
+
+impl TryFrom<EvmHostEvents> for ismp::events::Event {
+    type Error = anyhow::Error;
+    fn try_from(event: EvmHostEvents) -> Result<Self, Self::Error> {
+        match event {
+            EvmHostEvents::GetRequestEventFilter(get) =>
+                Ok(ismp::events::Event::GetRequest(router::Get {
+                    source: StateMachine::from_str(&String::from_utf8(get.source.0.into())?)
+                        .map_err(|e| anyhow!("{}", e))?,
+                    dest: StateMachine::from_str(&String::from_utf8(get.dest.0.into())?)
+                        .map_err(|e| anyhow!("{}", e))?,
+                    nonce: get.nonce.low_u64(),
+                    from: get.from.0.into(),
+                    keys: get.keys.into_iter().map(|key| key.0.into()).collect(),
+                    height: get.height.low_u64(),
+                    timeout_timestamp: get.timeout_timestamp.low_u64(),
+                    gas_limit: get.gaslimit.low_u64(),
+                })),
+            EvmHostEvents::PostRequestEventFilter(post) =>
+                Ok(ismp::events::Event::PostRequest(router::Post {
+                    source: StateMachine::from_str(&String::from_utf8(post.source.0.into())?)
+                        .map_err(|e| anyhow!("{}", e))?,
+                    dest: StateMachine::from_str(&String::from_utf8(post.dest.0.into())?)
+                        .map_err(|e| anyhow!("{}", e))?,
+                    nonce: post.nonce.low_u64(),
+                    from: post.from.0.into(),
+                    to: post.to.0.into(),
+                    timeout_timestamp: post.timeout_timestamp.low_u64(),
+                    data: post.data.0.into(),
+                    gas_limit: post.gaslimit.low_u64(),
+                })),
+            EvmHostEvents::PostResponseEventFilter(resp) =>
+                Ok(ismp::events::Event::PostResponse(router::PostResponse {
+                    post: router::Post {
+                        source: StateMachine::from_str(&String::from_utf8(resp.source.0.into())?)
+                            .map_err(|e| anyhow!("{}", e))?,
+                        dest: StateMachine::from_str(&String::from_utf8(resp.dest.0.into())?)
+                            .map_err(|e| anyhow!("{}", e))?,
+                        nonce: resp.nonce.low_u64(),
+                        from: resp.from.0.into(),
+                        to: resp.to.0.into(),
+                        timeout_timestamp: resp.timeout_timestamp.low_u64(),
+                        data: resp.data.0.into(),
+                        gas_limit: resp.gaslimit.low_u64(),
+                    },
+                    response: resp.response.0.into(),
+                    timeout_timestamp: resp.timeout_timestamp.low_u64(),
+                    gas_limit: resp.res_gaslimit.low_u64(),
+                })),
+            event => Err(anyhow!("Unsupported event {event:?}")),
+        }
     }
 }
 

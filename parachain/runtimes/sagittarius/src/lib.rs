d5be33e6a521830283f8f2393efdab5aa79d7b7d
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2024-03-01 20:09:42 +0100

    resolve: rename messier -> sagittarius

--- parachain/runtimes/sagittarius/src/lib.rs
+++ parachain/runtimes/sagittarius/src/lib.rs
@@ -211,8 +211,8 @@ impl_opaque_keys! {
 
 #[sp_version::runtime_version]
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-    spec_name: create_runtime_str!("messier"),
-    impl_name: create_runtime_str!("messier"),
+    spec_name: create_runtime_str!("sagittarius"),
+    impl_name: create_runtime_str!("sagittarius"),
     authoring_version: 1,
     spec_version: 100,
     impl_version: 0,

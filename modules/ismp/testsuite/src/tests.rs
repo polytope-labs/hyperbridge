5d51ea061d5674a119def2ae85e60907783281db
Ayomide Adebara <99181720+Adebara123@users.noreply.github.com>
2024-03-19 09:03:37 +0100

    Ayomide/testsuit (#115)

--- modules/ismp/testsuite/src/tests.rs
+++ modules/ismp/testsuite/src/tests.rs
@@ -1,7 +1,13 @@
+use ismp::host::{Ethereum, StateMachine};
+
 use crate::{
-    check_challenge_period, check_client_expiry, frozen_check,
+    check_challenge_period, check_client_expiry, check_request_source_and_destination,
+    frozen_check, mock_consensus_state_id, mock_proxy_consensus_state_id,
     mocks::{Host, MockDispatcher},
-    post_request_timeout_check, post_response_timeout_check, write_outgoing_commitments,
+    post_request_timeout_check, post_response_timeout_check,
+    prevent_request_timeout_on_proxy_with_known_state_machine,
+    prevent_response_timeout_on_proxy_with_known_state_machine, sanity_check_for_proxies,
+    write_outgoing_commitments,
 };
 use std::sync::Arc;
 
@@ -42,4 +48,34 @@ fn should_process_post_response_timeouts_correctly() {
     let host = Arc::new(Host::default());
     let dispatcher = MockDispatcher(host.clone());
     post_response_timeout_check(&*host, &dispatcher).unwrap()
+}
+
+#[test]
+fn should_prevent_request_timeout_on_proxy_with_known_state_machine() {
+    let host = Arc::new(Host::default());
+    let dispatcher = MockDispatcher(host.clone());
+    let proxy_state_machine = StateMachine::Kusama(2000);
+    let direct_conn_state_machine = StateMachine::Ethereum(Ethereum::ExecutionLayer);
+    prevent_request_timeout_on_proxy_with_known_state_machine(
+        &*host,
+        &dispatcher,
+        proxy_state_machine,
+        direct_conn_state_machine,
+    )
+    .unwrap()
+}
+
+#[test]
+fn should_prevent_response_timeout_on_proxy_with_known_state_machine() {
+    let host = Arc::new(Host::default());
+    let dispatcher = MockDispatcher(host.clone());
+    let proxy_state_machine = StateMachine::Kusama(2000);
+    let direct_conn_state_machine = StateMachine::Ethereum(Ethereum::ExecutionLayer);
+    prevent_response_timeout_on_proxy_with_known_state_machine(
+        &*host,
+        &dispatcher,
+        proxy_state_machine,
+        direct_conn_state_machine,
+    )
+    .unwrap()
 }

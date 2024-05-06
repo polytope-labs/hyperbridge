ee79b282f8eda1446c6a6dc574fc5f9f1145583e
omadoyeabraham <omadoyeabraham.dev@gmail.com>
2024-05-06 15:27:21 +0100

    Merged in Request entity work

--- indexers/src/handlers/events/evmHost/postRequest.event.handler.ts
+++ indexers/src/handlers/events/evmHost/postRequest.event.handler.ts
@@ -1,8 +1,9 @@
 import assert from "assert";
-import { SupportedChain } from "../../../types";
+import { RequestStatus, SupportedChain } from "../../../types";
 import { getEvmChainFromTransaction } from "../../../utils/chain.helpers";
 import { PostRequestEventLog } from "../../../types/abi-interfaces/EthereumHostAbi";
 import { HyperBridgeService } from "../../../services/hyperbridge.service";
+import { RequestService } from "../../../services/request.service";
 
 /**
  * Handles the PostRequest event from Evm Hosts
@@ -12,10 +13,44 @@ export async function handlePostRequestEvent(
 ): Promise<void> {
   assert(event.args, "No handlePostRequestEvent args");
   logger.info("Handling PostRequest event");
+  const { transaction, blockNumber, transactionHash, args } = event;
 
-  const { transaction } = event;
+  let { data, dest, fee, from, nonce, source, timeoutTimestamp, to } = args;
 
   const chain: SupportedChain = getEvmChainFromTransaction(transaction);
 
   await HyperBridgeService.handlePostRequestOrResponseEvent(chain, event);
+
+  // Compute the request commitment
+  let request_commitment = RequestService.computeRequestCommitment(
+    source,
+    dest,
+    BigInt(nonce.toString()),
+    BigInt(timeoutTimestamp.toString()),
+    from,
+    to,
+    data,
+  );
+
+  // Create the request entity
+  await RequestService.findOrCreate(
+    request_commitment,
+    data,
+    dest,
+    BigInt(fee.toString()),
+    from,
+    BigInt(nonce.toString()),
+    source,
+    RequestStatus.SOURCE,
+    BigInt(timeoutTimestamp.toString()),
+    to,
+  );
+
+  // Create request meta data entity
+  await RequestService.updateRequestStatus(
+    request_commitment,
+    RequestStatus.SOURCE,
+    BigInt(blockNumber),
+    transactionHash,
+  );
 }

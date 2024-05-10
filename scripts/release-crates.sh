98f8653efc0c1ee573302f5e4933b4e82f98d132
Seun Lanlege <seun@polytope.technology>
2024-05-10 15:40:22 +0000

    add script for releasing crates

---
+++ scripts/release-crates.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cargo release \
+-p ismp \
+-p mmr-primitives \
+-p pallet-hyperbridge \
+-p pallet-ismp \
+-p pallet-ismp-runtime-api \
+-p pallet-ismp-rpc \
+-p substrate-state-machine \
+-p ismp-parachain \
+-p ismp-parachain-runtime-api \
+-p ismp-parachain-inherent --execute

b4751f7b255f96e81844baf5a8d2d5293ae3fb87
David Salami <wizdave97@gmail.com>
2024-05-14 15:58:12 +0000

    update subxt types

--- modules/subxt/utils/src/gargantua.rs
+++ modules/subxt/utils/src/gargantua.rs
@@ -1392,16 +1392,17 @@ pub mod api {
 					&self,
 				) -> ::subxt::runtime_api::Payload<
 					types::BlockEvents,
-					::std::vec::Vec<runtime_types::pallet_ismp::events::Event>,
+					::std::vec::Vec<runtime_types::ismp::events::Event>,
 				> {
 					::subxt::runtime_api::Payload::new_static(
 						"IsmpRuntimeApi",
 						"block_events",
 						types::BlockEvents {},
 						[
-							37u8, 99u8, 198u8, 226u8, 175u8, 135u8, 50u8, 228u8, 87u8, 64u8, 9u8,
-							164u8, 13u8, 49u8, 13u8, 202u8, 221u8, 160u8, 194u8, 179u8, 142u8,
-							171u8, 65u8, 237u8, 214u8, 77u8, 216u8, 161u8, 98u8, 131u8, 8u8, 18u8,
+							231u8, 161u8, 150u8, 200u8, 116u8, 110u8, 78u8, 187u8, 94u8, 114u8,
+							239u8, 111u8, 218u8, 182u8, 19u8, 226u8, 84u8, 125u8, 251u8, 230u8,
+							0u8, 47u8, 193u8, 113u8, 123u8, 236u8, 44u8, 130u8, 118u8, 220u8, 55u8,
+							180u8,
 						],
 					)
 				}
@@ -1410,20 +1411,16 @@ pub mod api {
 					&self,
 				) -> ::subxt::runtime_api::Payload<
 					types::BlockEventsWithMetadata,
-					::std::vec::Vec<(
-						runtime_types::pallet_ismp::events::Event,
-						::core::primitive::u32,
-					)>,
+					::std::vec::Vec<(runtime_types::ismp::events::Event, ::core::primitive::u32)>,
 				> {
 					::subxt::runtime_api::Payload::new_static(
 						"IsmpRuntimeApi",
 						"block_events_with_metadata",
 						types::BlockEventsWithMetadata {},
 						[
-							226u8, 50u8, 123u8, 173u8, 231u8, 170u8, 107u8, 138u8, 246u8, 126u8,
-							219u8, 154u8, 145u8, 64u8, 29u8, 70u8, 194u8, 241u8, 10u8, 250u8,
-							134u8, 40u8, 86u8, 7u8, 141u8, 219u8, 29u8, 179u8, 136u8, 140u8, 152u8,
-							153u8,
+							130u8, 174u8, 87u8, 243u8, 138u8, 100u8, 127u8, 22u8, 172u8, 60u8,
+							185u8, 10u8, 140u8, 83u8, 243u8, 191u8, 83u8, 13u8, 71u8, 157u8, 61u8,
+							39u8, 51u8, 70u8, 26u8, 37u8, 43u8, 80u8, 134u8, 175u8, 233u8, 233u8,
 						],
 					)
 				}
@@ -2140,9 +2137,9 @@ pub mod api {
 			.hash();
 		runtime_metadata_hash ==
 			[
-				237u8, 173u8, 54u8, 234u8, 83u8, 244u8, 67u8, 206u8, 75u8, 214u8, 226u8, 101u8,
-				10u8, 224u8, 235u8, 202u8, 170u8, 84u8, 248u8, 5u8, 186u8, 60u8, 190u8, 248u8,
-				34u8, 103u8, 143u8, 12u8, 82u8, 162u8, 166u8, 88u8,
+				13u8, 65u8, 228u8, 0u8, 194u8, 77u8, 70u8, 198u8, 248u8, 22u8, 201u8, 22u8, 76u8,
+				62u8, 8u8, 50u8, 116u8, 43u8, 63u8, 82u8, 28u8, 233u8, 73u8, 87u8, 126u8, 35u8,
+				111u8, 216u8, 126u8, 6u8, 174u8, 114u8,
 			]
 	}
 	pub mod system {
@@ -3024,9 +3021,9 @@ pub mod api {
 						"Events",
 						vec![],
 						[
-							0u8, 47u8, 206u8, 36u8, 246u8, 67u8, 189u8, 26u8, 233u8, 89u8, 172u8,
-							201u8, 87u8, 109u8, 103u8, 38u8, 31u8, 219u8, 121u8, 174u8, 230u8,
-							159u8, 131u8, 170u8, 100u8, 191u8, 76u8, 43u8, 96u8, 205u8, 104u8, 2u8,
+							139u8, 96u8, 88u8, 32u8, 187u8, 15u8, 169u8, 178u8, 114u8, 70u8, 115u8,
+							164u8, 218u8, 238u8, 185u8, 214u8, 128u8, 152u8, 230u8, 60u8, 102u8,
+							125u8, 92u8, 36u8, 215u8, 55u8, 32u8, 226u8, 35u8, 133u8, 202u8, 254u8,
 						],
 					)
 				}
@@ -16253,6 +16250,43 @@ pub mod api {
 				# [codec (crate = :: subxt :: ext :: codec)]
 				#[decode_as_type(crate_path = ":: subxt :: ext :: scale_decode")]
 				#[encode_as_type(crate_path = ":: subxt :: ext :: scale_encode")]
+				pub enum Event {
+					#[codec(index = 0)]
+					StateMachineUpdated(runtime_types::ismp::events::StateMachineUpdated),
+					#[codec(index = 1)]
+					StateCommitmentVetoed(runtime_types::ismp::events::StateCommitmentVetoed),
+					#[codec(index = 2)]
+					PostRequest(runtime_types::ismp::router::Post),
+					#[codec(index = 3)]
+					PostResponse(runtime_types::ismp::router::PostResponse),
+					#[codec(index = 4)]
+					GetRequest(runtime_types::ismp::router::Get),
+					#[codec(index = 5)]
+					PostRequestHandled(runtime_types::ismp::events::RequestResponseHandled),
+					#[codec(index = 6)]
+					PostResponseHandled(runtime_types::ismp::events::RequestResponseHandled),
+					#[codec(index = 7)]
+					PostRequestTimeoutHandled(runtime_types::ismp::events::TimeoutHandled),
+					#[codec(index = 8)]
+					PostResponseTimeoutHandled(runtime_types::ismp::events::TimeoutHandled),
+					#[codec(index = 9)]
+					GetRequestHandled(runtime_types::ismp::events::RequestResponseHandled),
+					#[codec(index = 10)]
+					GetRequestTimeoutHandled(runtime_types::ismp::events::TimeoutHandled),
+				}
+				#[derive(
+					:: subxt :: ext :: codec :: Decode,
+					:: subxt :: ext :: codec :: Encode,
+					:: subxt :: ext :: scale_decode :: DecodeAsType,
+					:: subxt :: ext :: scale_encode :: EncodeAsType,
+					Clone,
+					Debug,
+					Eq,
+					PartialEq,
+				)]
+				# [codec (crate = :: subxt :: ext :: codec)]
+				#[decode_as_type(crate_path = ":: subxt :: ext :: scale_decode")]
+				#[encode_as_type(crate_path = ":: subxt :: ext :: scale_encode")]
 				pub struct Meta {
 					pub source: runtime_types::ismp::host::StateMachine,
 					pub dest: runtime_types::ismp::host::StateMachine,
@@ -16288,8 +16322,44 @@ pub mod api {
 				# [codec (crate = :: subxt :: ext :: codec)]
 				#[decode_as_type(crate_path = ":: subxt :: ext :: scale_decode")]
 				#[encode_as_type(crate_path = ":: subxt :: ext :: scale_encode")]
+				pub struct StateCommitmentVetoed {
+					pub height: runtime_types::ismp::consensus::StateMachineHeight,
+					pub fisherman: ::std::vec::Vec<::core::primitive::u8>,
+				}
+				#[derive(
+					:: subxt :: ext :: codec :: Decode,
+					:: subxt :: ext :: codec :: Encode,
+					:: subxt :: ext :: scale_decode :: DecodeAsType,
+					:: subxt :: ext :: scale_encode :: EncodeAsType,
+					Clone,
+					Debug,
+					Eq,
+					PartialEq,
+				)]
+				# [codec (crate = :: subxt :: ext :: codec)]
+				#[decode_as_type(crate_path = ":: subxt :: ext :: scale_decode")]
+				#[encode_as_type(crate_path = ":: subxt :: ext :: scale_encode")]
+				pub struct StateMachineUpdated {
+					pub state_machine_id: runtime_types::ismp::consensus::StateMachineId,
+					pub latest_height: ::core::primitive::u64,
+				}
+				#[derive(
+					:: subxt :: ext :: codec :: Decode,
+					:: subxt :: ext :: codec :: Encode,
+					:: subxt :: ext :: scale_decode :: DecodeAsType,
+					:: subxt :: ext :: scale_encode :: EncodeAsType,
+					Clone,
+					Debug,
+					Eq,
+					PartialEq,
+				)]
+				# [codec (crate = :: subxt :: ext :: codec)]
+				#[decode_as_type(crate_path = ":: subxt :: ext :: scale_decode")]
+				#[encode_as_type(crate_path = ":: subxt :: ext :: scale_encode")]
 				pub struct TimeoutHandled {
 					pub commitment: ::subxt::utils::H256,
+					pub source: runtime_types::ismp::host::StateMachine,
+					pub dest: runtime_types::ismp::host::StateMachine,
 				}
 			}
 			pub mod host {
@@ -18287,55 +18357,6 @@ pub mod api {
 					UnknownRequest { meta: runtime_types::ismp::events::Meta },
 					#[codec(index = 34)]
 					UnknownResponse { meta: runtime_types::ismp::events::Meta },
-				}
-			}
-			pub mod events {
-				use super::runtime_types;
-				#[derive(
-					:: subxt :: ext :: codec :: Decode,
-					:: subxt :: ext :: codec :: Encode,
-					:: subxt :: ext :: scale_decode :: DecodeAsType,
-					:: subxt :: ext :: scale_encode :: EncodeAsType,
-					Clone,
-					Debug,
-					Eq,
-					PartialEq,
-				)]
-				# [codec (crate = :: subxt :: ext :: codec)]
-				#[decode_as_type(crate_path = ":: subxt :: ext :: scale_decode")]
-				#[encode_as_type(crate_path = ":: subxt :: ext :: scale_encode")]
-				pub enum Event {
-					#[codec(index = 0)]
-					StateMachineUpdated {
-						state_machine_id: runtime_types::ismp::consensus::StateMachineId,
-						latest_height: ::core::primitive::u64,
-					},
-					#[codec(index = 1)]
-					Response {
-						dest_chain: runtime_types::ismp::host::StateMachine,
-						source_chain: runtime_types::ismp::host::StateMachine,
-						request_nonce: ::core::primitive::u64,
-						commitment: ::subxt::utils::H256,
-					},
-					#[codec(index = 2)]
-					Request {
-						dest_chain: runtime_types::ismp::host::StateMachine,
-						source_chain: runtime_types::ismp::host::StateMachine,
-						request_nonce: ::core::primitive::u64,
-						commitment: ::subxt::utils::H256,
-					},
-					#[codec(index = 3)]
-					PostRequestHandled(runtime_types::ismp::events::RequestResponseHandled),
-					#[codec(index = 4)]
-					PostResponseHandled(runtime_types::ismp::events::RequestResponseHandled),
-					#[codec(index = 5)]
-					GetRequestHandled(runtime_types::ismp::events::RequestResponseHandled),
-					#[codec(index = 6)]
-					PostRequestTimeoutHandled(runtime_types::ismp::events::TimeoutHandled),
-					#[codec(index = 7)]
-					PostResponseTimeoutHandled(runtime_types::ismp::events::TimeoutHandled),
-					#[codec(index = 8)]
-					GetRequestTimeoutHandled(runtime_types::ismp::events::TimeoutHandled),
 				}
 			}
 			pub mod mmr {

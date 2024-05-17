fc96cbee9ec54d9ca5888219b443bd972488cdfb
Seun Lanlege <seun@polytope.technology>
2024-05-17 10:15:31 +0000

    Regenesis paseo

--- parachain/runtimes/gargantua/src/lib.rs
+++ parachain/runtimes/gargantua/src/lib.rs
@@ -214,7 +214,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("gargantua"),
 	impl_name: create_runtime_str!("gargantua"),
 	authoring_version: 1,
-	spec_version: 240,
+	spec_version: 250,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,

9ba33f6dd0f4fc90d4351ebf8a39d3df36629ea2
David Salami <wizdave97@gmail.com>
2024-08-10 13:57:18 +0000

    initialize hyperbridge client at latest finalized height

--- tesseract/substrate/src/lib.rs
+++ tesseract/substrate/src/lib.rs
@@ -144,9 +144,8 @@ where
 		&mut self,
 		counterparty: Arc<dyn IsmpProvider>,
 	) -> Result<(), anyhow::Error> {
-		let id = self.state_machine_id();
 		let name = counterparty.name();
-		self.initial_height = counterparty.query_latest_height(id).await?.into();
+		self.initial_height = self.query_finalized_height().await?.into();
 		log::info!(
 			"Initialized height for {:?}->{name} at {}",
 			self.state_machine,

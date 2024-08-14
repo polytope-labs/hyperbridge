3788a6ba316f814985b72b3b8ded8207dcf356c2
dharjeezy <dharjeezy@gmail.com>
2024-08-14 05:36:30 +0100

    gargatuan runtime compiles

--- parachain/runtimes/gargantua/src/ismp.rs
+++ parachain/runtimes/gargantua/src/ismp.rs
@@ -42,7 +42,7 @@ use ismp::router::Timeout;
 use ismp_sync_committee::constants::sepolia::Sepolia;
 use pallet_ismp::{dispatcher::FeeMetadata, ModuleId};
 use sp_std::prelude::*;
-use staging_xcm::latest::MultiLocation;
+use staging_xcm::latest::Location;
 
 #[derive(Default)]
 pub struct ProxyModule;
@@ -162,8 +162,8 @@ parameter_types! {
 impl pallet_assets::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type Balance = Balance;
-	type AssetId = MultiLocation;
-	type AssetIdParameter = MultiLocation;
+	type AssetId = Location;
+	type AssetIdParameter = Location;
 	type Currency = Balances;
 	type CreateOrigin = AsEnsureOriginWithArg<frame_system::EnsureSigned<AccountId32>>;
 	type ForceOrigin = EnsureRoot<AccountId32>;

6cce53859d50eb3dbfa854c04d577fbeeacbbe0a
David Salami <wizdave97@gmail.com>
2024-08-23 17:48:06 +0000

    enable ismp grandpa components

--- modules/consensus/grandpa/verifier/src/lib.rs
+++ modules/consensus/grandpa/verifier/src/lib.rs
@@ -28,9 +28,9 @@ use alloc::collections::BTreeMap;
 use anyhow::anyhow;
 use codec::Decode;
 use finality_grandpa::Chain;
-use primitives::{
-    justification::{find_scheduled_change, AncestryChain, GrandpaJustification},
-    parachain_header_storage_key, ConsensusState, FinalityProof, ParachainHeadersWithFinalityProof,
+use grandpa_verifier_primitives::{
+	justification::{find_scheduled_change, AncestryChain, GrandpaJustification},
+	parachain_header_storage_key, ConsensusState, FinalityProof, ParachainHeadersWithFinalityProof,
 };
 use sp_core::H256;
 use sp_runtime::traits::{BlakeTwo256, Header};
@@ -41,67 +41,67 @@ use substrate_state_machine::read_proof_check;
 /// This function verifies the GRANDPA finality proof for both standalone chain and parachain
 /// headers.
 pub fn verify_grandpa_finality_proof<H>(
-    mut consensus_state: ConsensusState,
-    finality_proof: FinalityProof<H>,
+	mut consensus_state: ConsensusState,
+	finality_proof: FinalityProof<H>,
 ) -> Result<(ConsensusState, H, Vec<H256>, AncestryChain<H>), anyhow::Error>
 where
-    H: Header<Hash = H256, Number = u32>,
-    H::Number: finality_grandpa::BlockNumberOps + Into<u32>,
+	H: Header<Hash = H256, Number = u32>,
+	H::Number: finality_grandpa::BlockNumberOps + Into<u32>,
 {
-    // First validate unknown headers.
-    let headers = AncestryChain::<H>::new(&finality_proof.unknown_headers);
+	// First validate unknown headers.
+	let headers = AncestryChain::<H>::new(&finality_proof.unknown_headers);
 
-    let target = finality_proof
-        .unknown_headers
-        .iter()
-        .max_by_key(|h| *h.number())
-        .ok_or_else(|| anyhow!("Unknown headers can't be empty!"))?;
+	let target = finality_proof
+		.unknown_headers
+		.iter()
+		.max_by_key(|h| *h.number())
+		.ok_or_else(|| anyhow!("Unknown headers can't be empty!"))?;
 
-    // this is illegal
-    if target.hash() != finality_proof.block {
-        Err(anyhow!("Latest finalized block should be highest block in unknown_headers"))?;
-    }
+	// this is illegal
+	if target.hash() != finality_proof.block {
+		Err(anyhow!("Latest finalized block should be highest block in unknown_headers"))?;
+	}
 
-    let justification = GrandpaJustification::<H>::decode(&mut &finality_proof.justification[..])
-        .map_err(|e| anyhow!("Failed to decode justificatio {:?}", e))?;
+	let justification = GrandpaJustification::<H>::decode(&mut &finality_proof.justification[..])
+		.map_err(|e| anyhow!("Failed to decode justificatio {:?}", e))?;
 
-    if justification.commit.target_hash != finality_proof.block {
-        Err(anyhow!("Justification target hash and finality proof block hash mismatch"))?;
-    }
+	if justification.commit.target_hash != finality_proof.block {
+		Err(anyhow!("Justification target hash and finality proof block hash mismatch"))?;
+	}
 
-    let from = consensus_state.latest_hash;
+	let from = consensus_state.latest_hash;
 
-    let base = finality_proof
-        .unknown_headers
-        .iter()
-        .min_by_key(|h| *h.number())
-        .ok_or_else(|| anyhow!("Unknown headers can't be empty!"))?;
+	let base = finality_proof
+		.unknown_headers
+		.iter()
+		.min_by_key(|h| *h.number())
+		.ok_or_else(|| anyhow!("Unknown headers can't be empty!"))?;
 
-    if base.number() < &consensus_state.latest_height {
-        headers.ancestry(base.hash(), consensus_state.latest_hash).map_err(|_| {
-            anyhow!(
-                "[verify_grandpa_finality_proof] Invalid ancestry (base -> latest relay block)!"
-            )
-        })?;
-    }
+	if base.number() < &consensus_state.latest_height {
+		headers.ancestry(base.hash(), consensus_state.latest_hash).map_err(|_| {
+			anyhow!(
+				"[verify_grandpa_finality_proof] Invalid ancestry (base -> latest relay block)!"
+			)
+		})?;
+	}
 
-    let mut finalized = headers
-        .ancestry(from, target.hash())
-        .map_err(|_| anyhow!("[verify_grandpa_finality_proof] Invalid ancestry!"))?;
-    finalized.sort();
+	let mut finalized = headers
+		.ancestry(from, target.hash())
+		.map_err(|_| anyhow!("[verify_grandpa_finality_proof] Invalid ancestry!"))?;
+	finalized.sort();
 
-    // 2. verify justification.
-    justification.verify(consensus_state.current_set_id, &consensus_state.current_authorities)?;
+	// 2. verify justification.
+	justification.verify(consensus_state.current_set_id, &consensus_state.current_authorities)?;
 
-    // Sets new consensus state, optionally rotating authorities
-    consensus_state.latest_hash = target.hash();
-    consensus_state.latest_height = (*target.number()).into();
-    if let Some(scheduled_change) = find_scheduled_change::<H>(&target) {
-        consensus_state.current_set_id += 1;
-        consensus_state.current_authorities = scheduled_change.next_authorities;
-    }
+	// Sets new consensus state, optionally rotating authorities
+	consensus_state.latest_hash = target.hash();
+	consensus_state.latest_height = (*target.number()).into();
+	if let Some(scheduled_change) = find_scheduled_change::<H>(&target) {
+		consensus_state.current_set_id += 1;
+		consensus_state.current_authorities = scheduled_change.next_authorities;
+	}
 
-    Ok((consensus_state, target.clone(), finalized, headers))
+	Ok((consensus_state, target.clone(), finalized, headers))
 }
 /// This function verifies the GRANDPA finality proof for relay chain headers.
 ///
@@ -109,59 +109,61 @@ where
 /// of these headers, stored at the recently finalized relay chain heights.
 /// Returns the new Consensus state alongside a map of para id to a vector that contains a tuple of
 /// finalized parachain header and timestamp
-pub fn verify_parachain_headers_with_grandpa_finality_proof<H>(
-    consensus_state: ConsensusState,
-    proof: ParachainHeadersWithFinalityProof<H>,
+pub fn verify_parachain_headers_with_grandpa_finality_proof<H, F>(
+	consensus_state: ConsensusState,
+	proof: ParachainHeadersWithFinalityProof<H>,
+	is_resgistered_para: F,
 ) -> Result<(ConsensusState, BTreeMap<u32, Vec<H>>), anyhow::Error>
 where
-    H: Header<Hash = H256, Number = u32>,
-    H::Number: finality_grandpa::BlockNumberOps + Into<u32>,
+	H: Header<Hash = H256, Number = u32>,
+	H::Number: finality_grandpa::BlockNumberOps + Into<u32>,
+	F: Fn(u32) -> bool,
 {
-    let ParachainHeadersWithFinalityProof { finality_proof, parachain_headers } = proof;
+	let ParachainHeadersWithFinalityProof { finality_proof, parachain_headers } = proof;
 
-    let (consensus_state, _, finalized_hashes, headers) =
-        verify_grandpa_finality_proof(consensus_state, finality_proof)?;
-    // verifies state proofs of parachain headers in finalized relay chain headers.
-    let mut verified_parachain_headers: BTreeMap<u32, Vec<H>> = BTreeMap::new();
-    for (hash, proof) in parachain_headers {
-        if finalized_hashes.binary_search(&hash).is_err() {
-            // seems relay hash isn't in the finalized chain.
-            continue
-        }
-        let relay_chain_header =
-            headers.header(&hash).expect("Headers have been checked by AncestryChain; qed");
-        let state_proof = proof.state_proof;
-        let mut keys = BTreeMap::new();
-        for para_id in proof.para_ids {
-            // ensure the para id is in the consensus state before proof verification
-            if !consensus_state.para_ids.contains_key(&para_id) {
-                continue
-            }
+	let (consensus_state, _, finalized_hashes, headers) =
+		verify_grandpa_finality_proof(consensus_state, finality_proof)?;
+	// verifies state proofs of parachain headers in finalized relay chain headers.
+	let mut verified_parachain_headers: BTreeMap<u32, Vec<H>> = BTreeMap::new();
+	for (hash, proof) in parachain_headers {
+		if finalized_hashes.binary_search(&hash).is_err() {
+			// seems relay hash isn't in the finalized chain.
+			continue
+		}
+		let relay_chain_header =
+			headers.header(&hash).expect("Headers have been checked by AncestryChain; qed");
+		let state_proof = proof.state_proof;
+		let mut keys = BTreeMap::new();
+		for para_id in proof.para_ids {
+			// ensure the para id is in the consensus state before proof verification
+			if !is_resgistered_para(para_id) {
+				continue
+			}
 
-            let key = parachain_header_storage_key(para_id);
+			let key = parachain_header_storage_key(para_id);
 
-            keys.insert(key.0, para_id);
-        }
+			keys.insert(key.0, para_id);
+		}
 
-        let proof = StorageProof::new(state_proof);
+		let proof = StorageProof::new(state_proof);
 
-        // verify patricia-merkle state proofs
-        let mut result = read_proof_check::<BlakeTwo256, _>(
-            relay_chain_header.state_root(),
-            proof,
-            keys.keys().map(|key| key.as_slice()),
-        )
-        .map_err(|err| anyhow!("error verifying parachain header state proof: {err:?}"))?;
-        for (key, para_id) in keys {
-            let header = result
-                .remove(&key)
-                .flatten()
-                .ok_or_else(|| anyhow!("Invalid proof, parachain header not found"))?;
-            let parachain_header =
-                H::decode(&mut &header[..]).map_err(|e| anyhow!("error decoding header: {e:?}"))?;
-            verified_parachain_headers.entry(para_id).or_default().push(parachain_header);
-        }
-    }
+		// verify patricia-merkle state proofs
+		let mut result = read_proof_check::<BlakeTwo256, _>(
+			relay_chain_header.state_root(),
+			proof,
+			keys.keys().map(|key| key.as_slice()),
+		)
+		.map_err(|err| anyhow!("error verifying parachain header state proof: {err:?}"))?;
+		for (key, para_id) in keys {
+			let header = result
+				.remove(&key)
+				.flatten()
+				.ok_or_else(|| anyhow!("Invalid proof, parachain header not found"))?;
+			let parachain_header =
+				H::decode(&mut &header[..]).map_err(|e| anyhow!("error decoding header: {e:?}"))?;
+			verified_parachain_headers.entry(para_id).or_default().push(parachain_header);
+		}
+	}
 
-    Ok((consensus_state, verified_parachain_headers))
+	Ok((consensus_state, verified_parachain_headers))
 }
---
+++ modules/ismp/clients/grandpa/src/lib.rs
@@ -0,0 +1,181 @@
+// Copyright (c) 2024 Polytope Labs.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific lang
+
+#![cfg_attr(not(feature = "std"), no_std)]
+extern crate alloc;
+
+pub mod consensus;
+pub mod messages;
+
+use alloc::vec::Vec;
+use ismp::consensus::ConsensusStateId;
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use super::*;
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+	use ismp::{
+		consensus::StateMachineId,
+		host::{IsmpHost, StateMachine},
+	};
+
+	#[pallet::pallet]
+	#[pallet::without_storage_info]
+	pub struct Pallet<T>(_);
+
+	/// The config trait
+	#[pallet::config]
+	pub trait Config: frame_system::Config + pallet_ismp::Config {
+		/// The overarching event type
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+
+		type IsmpHost: IsmpHost + Default;
+	}
+
+	/// Events emitted by this pallet
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		/// Parachains with the `para_ids` have been added to the whitelist
+		ParachainsAdded {
+			/// The parachains in question
+			para_ids: Vec<(u32, u64)>,
+		},
+		/// Parachains with the `para_ids` have been removed from the whitelist
+		ParachainsRemoved {
+			/// The parachains in question
+			para_ids: Vec<u32>,
+		},
+		/// Standalone Chain Added to whitelist
+		StandaloneChainsAdded {
+			/// The state machines in question
+			state_machines: Vec<StateMachine>,
+		},
+		/// Standalone have been removed from the whitelist
+		StandaloneChainsRemoved {
+			/// The state machines in question
+			state_machines: Vec<StateMachine>,
+		},
+	}
+
+	#[pallet::error]
+	pub enum Error<T> {
+		/// Standalone Consensus State Already Exists
+		StandaloneConsensusStateAlreadyExists,
+		/// Standalone Consensus Does not Exist
+		StandaloneConsensusStateDontExists,
+		/// Error fetching consensus state
+		ErrorFetchingConsensusState,
+		/// Error decoding consensus state
+		ErrorDecodingConsensusState,
+		/// Incorrect consensus state id length
+		IncorrectConsensusStateIdLength,
+		/// Error storing consensus state
+		ErrorStoringConsensusState,
+	}
+
+	/// Registered Standalone chains for the grandpa consensus client
+	#[pallet::storage]
+	#[pallet::getter(fn state_machines)]
+	pub type SupportedStateMachines<T: Config> =
+		StorageMap<_, Twox64Concat, StateMachine, bool, OptionQuery>;
+
+	/// List of parachains that this state machine is interested in.
+	#[pallet::storage]
+	pub type Parachains<T: Config> = StorageMap<_, Identity, u32, u64>;
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		/// Add some a state machine to the list of supported state machines
+		#[pallet::call_index(0)]
+		#[pallet::weight((0, DispatchClass::Mandatory))]
+		pub fn add_state_machines(
+			origin: OriginFor<T>,
+			state_machines: Vec<StateMachine>,
+		) -> DispatchResult {
+			T::AdminOrigin::ensure_origin(origin)?;
+
+			for state_machine in state_machines {
+				SupportedStateMachines::<T>::insert(state_machine, true)
+			}
+
+			Ok(())
+		}
+
+		/// Remove a state machine from the list of supported state machines
+		#[pallet::call_index(1)]
+		#[pallet::weight((0, DispatchClass::Mandatory))]
+		pub fn remove_state_machine(
+			origin: OriginFor<T>,
+			state_machines: Vec<StateMachine>,
+		) -> DispatchResult {
+			T::AdminOrigin::ensure_origin(origin)?;
+
+			for state_machine in state_machines.clone() {
+				SupportedStateMachines::<T>::remove(state_machine)
+			}
+
+			Self::deposit_event(Event::StandaloneChainsRemoved { state_machines });
+
+			Ok(())
+		}
+
+		/// Add some new parachains to the parachains whitelist
+		#[pallet::call_index(2)]
+		#[pallet::weight(<T as frame_system::Config>::DbWeight::get().writes(data.parachains.len() as u64))]
+		pub fn add_parachain(origin: OriginFor<T>, data: ParachainData) -> DispatchResult {
+			T::AdminOrigin::ensure_origin(origin)?;
+			let host = <T::IsmpHost>::default();
+			for (para, slot_duration) in &data.parachains {
+				let state_id = match <T as pallet_ismp::Config>::Coprocessor::get() {
+					Some(StateMachine::Kusama(_)) => StateMachine::Kusama(*para),
+					Some(StateMachine::Polkadot(_)) => StateMachine::Polkadot(*para),
+					_ => continue,
+				};
+				Parachains::<T>::insert(*para, slot_duration);
+				let _ = host.store_challenge_period(
+					StateMachineId { state_id, consensus_state_id: data.consensus_state_id },
+					data.challenge_period,
+				);
+			}
+
+			Self::deposit_event(Event::ParachainsAdded { para_ids: data.parachains });
+
+			Ok(())
+		}
+
+		/// Removes some parachains from the parachains whitelist
+		#[pallet::call_index(3)]
+		#[pallet::weight(<T as frame_system::Config>::DbWeight::get().writes(para_ids.len() as u64))]
+		pub fn remove_parachain(origin: OriginFor<T>, para_ids: Vec<u32>) -> DispatchResult {
+			T::AdminOrigin::ensure_origin(origin)?;
+			for id in &para_ids {
+				Parachains::<T>::remove(id);
+			}
+
+			Self::deposit_event(Event::ParachainsRemoved { para_ids });
+
+			Ok(())
+		}
+	}
+}
+
+#[derive(Clone, codec::Encode, codec::Decode, scale_info::TypeInfo, Debug, PartialEq, Eq)]
+pub struct ParachainData {
+	pub consensus_state_id: ConsensusStateId,
+	pub parachains: Vec<(u32, u64)>,
+	pub challenge_period: u64,
+}

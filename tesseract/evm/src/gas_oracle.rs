5458ddf5187bcd301917cf9fdcda1acdfec483f9
David Salami <wizdave97@gmail.com>
2024-09-16 11:44:39 +0000

    nit

--- tesseract/evm/src/gas_oracle.rs
+++ tesseract/evm/src/gas_oracle.rs
@@ -184,7 +184,7 @@ pub async fn get_current_gas_cost_in_usd(
 						let oracle_gas_price = parse_units(response_json.average, "gwei")?.into();
 						gas_price = std::cmp::max(node_gas_price, oracle_gas_price);
 					}
-					// Gnosis uses a stable coin for gas convert the gas which means the usd is
+					// Gnosis uses a stable coin for gas token which means the usd is
 					// equivalent to the gas price
 					gas_price_cost = gas_price
 				},

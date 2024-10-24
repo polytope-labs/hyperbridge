aef694862ef7b19cda7af4c272dbba6eb3a145b5
David Salami <wizdave97@gmail.com>
2024-10-24 16:47:32 +0000

    fix xcm configuration

--- parachain/runtimes/nexus/src/lib.rs
+++ parachain/runtimes/nexus/src/lib.rs
@@ -217,7 +217,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("nexus"),
 	impl_name: create_runtime_str!("nexus"),
 	authoring_version: 1,
-	spec_version: 1000,
+	spec_version: 1100,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,

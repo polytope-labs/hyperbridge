7c8a7a8127da5f49973271d17fd74c46b3f3aa1c
David Salami <wizdave97@gmail.com>
2024-10-29 21:23:57 +0000

    enable metadata build in substrate wasm builder

--- parachain/runtimes/gargantua/src/lib.rs
+++ parachain/runtimes/gargantua/src/lib.rs
@@ -233,7 +233,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("gargantua"),
 	impl_name: create_runtime_str!("gargantua"),
 	authoring_version: 1,
-	spec_version: 1190,
+	spec_version: 1200,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,

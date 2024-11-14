2e4ca442d3d88040dc6e7a3b4e505276210e3b1f
David Salami <wizdave97@gmail.com>
2024-11-14 12:23:37 +0000

    fix ismp router

--- parachain/runtimes/nexus/src/ismp.rs
+++ parachain/runtimes/nexus/src/ismp.rs
@@ -249,7 +249,7 @@ impl IsmpModule for ProxyModule {
 				(&post.from, &post.source, &post.dest)
 			},
 			Timeout::Request(Request::Get(get)) => (&get.from, &get.source, &get.dest),
-			Timeout::Response(res) => (&res.post.to, &res.post.dest, &res.post.dest),
+			Timeout::Response(res) => (&res.post.to, &res.source_chain(), &res.dest_chain()),
 		};
 
 		if *source != HostStateMachine::get() {

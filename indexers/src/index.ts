7896cc6a39e417dc983e4d29b68fc0f9a98e2f19
Seun Lanlege <seun@polytope.technology>
2024-12-13 11:35:33 +0000

    remove the indexer code

--- indexers/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { URL } from "url";
-// @ts-ignore
-global.URL = URL;
-
-// Required for ethers to work in node
-import "@ethersproject/shims";
-
-//Exports all handler functions
-export * from "./mappings/mappingHandlers";

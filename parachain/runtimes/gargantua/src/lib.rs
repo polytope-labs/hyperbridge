d8a726d6ba416756e9283c1cb23d2ac64878e5d2
David Salami <wizdave97@gmail.com>
2025-01-24 22:24:25 +0400

    publish hyperclient

--- parachain/runtimes/gargantua/src/lib.rs
+++ parachain/runtimes/gargantua/src/lib.rs
@@ -234,7 +234,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("gargantua"),
 	impl_name: create_runtime_str!("gargantua"),
 	authoring_version: 1,
-	spec_version: 1_600,
+	spec_version: 1_700,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,

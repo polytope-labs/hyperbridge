6312756eb91bcf104279e9258e113295edd7f411
Desmond Obisi <desmondcutest@yahoo.com>
2025-01-28 13:39:02 +0100

    Update src/utils/substrate.helpers.ts
    Co-authored-by: David Salami <31099392+Wizdave97@users.noreply.github.com>

--- sdk/src/utils/substrate.helpers.ts
+++ sdk/src/utils/substrate.helpers.ts
@@ -43,7 +43,6 @@ export const extractStateMachineIdFromSubstrateEventData = (
 
   switch (main_key) {
    case 'EVM':
-   case 'ETHEREUM':
     return 'EVM-'.concat(value);
    case 'POLKADOT':
     return 'POLKADOT-'.concat(value);

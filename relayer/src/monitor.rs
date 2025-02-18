41007b74b019770da479579237a7b0c7fa3b646e
David Salami <wizdave97@gmail.com>
2025-02-18 12:12:19 +0000

    update monitoring task to watch for updates to counterparty chains

--- relayer/src/monitor.rs
+++ relayer/src/monitor.rs
@@ -1,5 +1,9 @@
-use ismp::consensus::{StateMachineHeight, StateMachineId};
-use std::time::Duration;
+use anyhow::anyhow;
+use ismp::{
+	consensus::{StateMachineHeight, StateMachineId},
+	host::StateMachine,
+};
+use std::{collections::HashMap, sync::Arc, time::Duration};
 use tesseract_primitives::IsmpHost;
 use tesseract_substrate::config::{Blake2SubstrateChain, KeccakSubstrateChain};
 
@@ -7,13 +11,14 @@ use crate::any::HyperbridgeHostConfig;
 
 pub async fn monitor_clients(
 	hyperbridge_config: HyperbridgeHostConfig,
+	client_map: HashMap<StateMachine, Arc<dyn IsmpHost>>,
 	configs: Vec<(StateMachineId, u64)>,
 ) -> anyhow::Result<()> {
 	let hyperbridge = hyperbridge_config
 		.clone()
 		.into_client::<Blake2SubstrateChain, KeccakSubstrateChain>()
 		.await?;
-	let provider = hyperbridge.provider();
+	let hyperbride_provider = hyperbridge.provider();
 
 	enum HealthStat {
 		Restart,
@@ -22,9 +27,41 @@ pub async fn monitor_clients(
 
 	let lambda = || async {
 		for (id, max_interval) in configs.clone() {
-			log::trace!(target: "tesseract", "Checking update interval for {:?}", id.state_id);
-			let latest_height = provider.query_latest_height(id).await?;
+			log::trace!(target: "tesseract", "Checking update interval for {:?} on {:?}", id.state_id, hyperbride_provider.state_machine_id().state_id);
+			let latest_height = hyperbride_provider.query_latest_height(id).await?;
 			let state_machine_height = StateMachineHeight { id, height: latest_height.into() };
+			let last_state_machine_update_time = hyperbride_provider
+				.query_state_machine_update_time(state_machine_height)
+				.await?;
+
+			let current_timestamp = hyperbride_provider.query_timestamp().await?;
+
+			if current_timestamp
+				.as_secs()
+				.saturating_sub(last_state_machine_update_time.as_secs()) >=
+				max_interval
+			{
+				log::trace!(target: "tesseract", "{:?} -> {:?} Has stalled shutting down", id.state_id, hyperbride_provider.state_machine_id().state_id);
+				return Ok::<_, anyhow::Error>(HealthStat::Restart);
+			}
+
+			// Don't check update interval for hyperbridge on evm chains
+			if id.state_id.is_evm() {
+				return Ok::<_, anyhow::Error>(HealthStat::Ok);
+			}
+
+			log::trace!(target: "tesseract", "Checking update interval for {:?} on {:?}", hyperbride_provider.state_machine_id().state_id, id.state_id);
+			let provider = client_map
+				.get(&id.state_id)
+				.cloned()
+				.ok_or_else(|| anyhow!("Client for {:?} not found in config", id.state_id))?
+				.provider();
+			let latest_height =
+				provider.query_latest_height(hyperbride_provider.state_machine_id()).await?;
+			let state_machine_height = StateMachineHeight {
+				id: hyperbride_provider.state_machine_id(),
+				height: latest_height.into(),
+			};
 			let last_state_machine_update_time =
 				provider.query_state_machine_update_time(state_machine_height).await?;
 
@@ -35,7 +72,7 @@ pub async fn monitor_clients(
 				.saturating_sub(last_state_machine_update_time.as_secs()) >=
 				max_interval
 			{
-				log::trace!(target: "tesseract", "{:?} Has stalled shutting down", id.state_id);
+				log::trace!(target: "tesseract", "{:?} -> {:?} Has stalled shutting down", hyperbride_provider.state_machine_id().state_id, id.state_id);
 				return Ok::<_, anyhow::Error>(HealthStat::Restart);
 			}
 		}

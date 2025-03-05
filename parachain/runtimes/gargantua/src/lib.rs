4398bca4570d0ce7bd3e0dd80ac5848d8deaff99
David Salami <wizdave97@gmail.com>
2025-03-05 13:34:47 +0000

    gnosis pectra

--- parachain/runtimes/gargantua/src/lib.rs
+++ parachain/runtimes/gargantua/src/lib.rs
@@ -235,7 +235,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("gargantua"),
 	impl_name: create_runtime_str!("gargantua"),
 	authoring_version: 1,
-	spec_version: 2_400,
+	spec_version: 2_500,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,

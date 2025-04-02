b9ae4985a761d948e59cdaec13a465fd08427c83
David Salami <31099392+Wizdave97@users.noreply.github.com>
2025-04-02 19:05:46 +0300

    Auto generate constants from config (#34)
    * add argon chainid
    
    * autogenerate constants
    
    * minor fix

--- sdk/packages/indexer/scripts/generate-chain-yamls.js
+++ sdk/packages/indexer/scripts/generate-chain-yamls.js
@@ -47,16 +47,18 @@ const generateSubstrateYaml = async (chain, config) => {
 	const header = await rpc.call("chain_getHeader", [])
 	const blockNumber = currentEnv === "local" ? hexToNumber(header.number) : config.startBlock
 	const chainTypesSection = chainTypesConfig ? `\n  chaintypes:\n    file: ${chainTypesConfig}` : ""
-	
+
 	// Check if this is a Hyperbridge chain (stateMachineId is KUSAMA-4009 or POLKADOT-3367)
 	const isHyperbridgeChain = config.stateMachineId === "KUSAMA-4009" || config.stateMachineId === "POLKADOT-3367"
-	
+
 	// Add AssetTeleported handler only for Hyperbridge chains
-	const assetTeleportedHandler = isHyperbridgeChain ? `        - handler: handleSubstrateAssetTeleportedEvent
+	const assetTeleportedHandler = isHyperbridgeChain
+		? `        - handler: handleSubstrateAssetTeleportedEvent
           kind: substrate/EventHandler
           filter:
             module: xcmGateway
-            method: AssetTeleported` : ''
+            method: AssetTeleported`
+		: ""
 
 	return `# // Auto-generated , DO NOT EDIT
 specVersion: 1.0.0
@@ -116,7 +118,7 @@ dataSources:
           kind: substrate/EventHandler
           filter:
             module: ismp
-            method: PostResponseTimeoutHandled${assetTeleportedHandler ? '\n' + assetTeleportedHandler : ''}
+            method: PostResponseTimeoutHandled${assetTeleportedHandler ? "\n" + assetTeleportedHandler : ""}
 
 repository: 'https://github.com/polytope-labs/hyperbridge'`
 }
@@ -303,10 +305,45 @@ const generateSubstrateWsJson = () => {
 	console.log("Generated substrate-ws.json")
 }
 
+const generateChainIdsByGenesis = () => {
+	const chainIdsByGenesis = {}
+
+	validChains.forEach(([chain, config]) => {
+		if (config.chainId) {
+			chainIdsByGenesis[config.chainId] = config.stateMachineId
+		}
+	})
+
+	const chainIdsByGenesisContent = `// Auto-generated, DO NOT EDIT
+export const CHAIN_IDS_BY_GENESIS = ${JSON.stringify(chainIdsByGenesis, null, 2)}`
+
+	fs.writeFileSync(root + "/src/chain-ids-by-genesis.ts", chainIdsByGenesisContent)
+	console.log("Generated chain-ids-by-genesis.ts")
+}
+
+const generateChainsByIsmpHost = () => {
+	const chainsByIsmpHost = {}
+
+	validChains.forEach(([chain, config]) => {
+		// Only include EVM chains with ethereumHost contract
+		if (config.type === "evm" && config.contracts?.ethereumHost) {
+			chainsByIsmpHost[config.stateMachineId] = config.contracts.ethereumHost
+		}
+	})
+
+	const chainsByIsmpHostContent = `// Auto-generated, DO NOT EDIT
+export const CHAINS_BY_ISMP_HOST = ${JSON.stringify(chainsByIsmpHost, null, 2)}`
+
+	fs.writeFileSync(root + "/src/chains-by-ismp-host.ts", chainsByIsmpHostContent)
+	console.log("Generated chains-by-ismp-host.ts")
+}
+
 generateAllChainYamls()
 	.then(() => {
 		generateMultichainYaml()
 		generateSubstrateWsJson()
+		generateChainIdsByGenesis()
+		generateChainsByIsmpHost()
 		process.exit(0)
 	})
 	.catch((err) => {
--- sdk/packages/indexer/src/constants.ts
+++ sdk/packages/indexer/src/constants.ts
@@ -56,76 +56,11 @@ export const SONEMIUM = {
 
 export const SUBSTRATE_RPC_URL = require("./substrate-ws.json")
 
-export const CHAIN_IDS_BY_GENESIS = {
-	// Hyperbridge
-	"0x5388faf792c5232566d21493929b32c1f20a9c2b03e95615eefec2aa26d64b73": "KUSAMA-4009",
-	"0x61ea8a51fd4a058ee8c0e86df0a89cc85b8b67a0a66432893d09719050c9f540": "POLKADOT-3367",
+import { CHAIN_IDS_BY_GENESIS } from "./chain-ids-by-genesis"
+import { CHAINS_BY_ISMP_HOST } from "./chains-by-ismp-host"
+export { CHAIN_IDS_BY_GENESIS, CHAINS_BY_ISMP_HOST }
 
-	// Bifrost
-	"0xec39b15e5a1945ff19b8e8c0f76990b5758ce19faa4578e8ed57eda33e844452": "KUSAMA-2030",
-	"0x262e1b2ad728475fd6fe88e62d34c200abe6fd693931ddad144059b1eb884e5b": "POLKADOT-2030",
-
-	// cere network
-	"0x81443836a9a24caaa23f1241897d1235717535711d1d3fe24eae4fdc942c092c": "SUBSTRATE-cere",
-
-	// BSC
-	"97": "EVM-97",
-	"56": "EVM-56",
-
-	// Ethereum
-	"11155111": "EVM-11155111",
-	"1": "EVM-1",
-
-	// Arbitrum
-	"421614": "EVM-421614",
-	"42161": "EVM-42161",
-
-	// Optimism
-	"11155420": "EVM-11155420",
-	"10": "EVM-10",
-
-	// Base
-	"84532": "EVM-84532",
-	"8453": "EVM-8453",
-
-	// Gnosis
-	"10200": "EVM-10200",
-	"100": "EVM-100",
-
-	//Sonemium
-	"1946": "EVM-1946",
-	"1868": "EVM-1868",
-}
-
-export const CHAINS_BY_ISMP_HOST = {
-	// Base
-	"EVM-84532": "0xD198c01839dd4843918617AfD1e4DDf44Cc3BB4a",
-	"EVM-8453": "0x6FFe92e4d7a9D589549644544780e6725E84b248",
-
-	// BSC
-	"EVM-97": "0x8Aa0Dea6D675d785A882967Bf38183f6117C09b7",
-	"EVM-56": "0x24B5d421Ec373FcA57325dd2F0C074009Af021F7",
-
-	// Arbitrum
-	"EVM-421614": "0x3435bD7e5895356535459D6087D1eB982DAd90e7",
-	"EVM-42161": "0xE05AFD4Eb2ce6d65c40e1048381BD0Ef8b4B299e",
-
-	// Optimism
-	"EVM-11155420": "0x6d51b678836d8060d980605d2999eF211809f3C2",
-	"EVM-10": "0x78c8A5F27C06757EA0e30bEa682f1FD5C8d7645d",
-
-	// Ethereum
-	"EVM-11155111": "0x2EdB74C269948b60ec1000040E104cef0eABaae8",
-	"EVM-1": "0x792A6236AF69787C40cF76b69B4c8c7B28c4cA20",
-
-	// Gnosis
-	"EVM-10200": "0x58A41B89F4871725E5D898d98eF4BF917601c5eB",
-	"EVM-100": "0x50c236247447B9d4Ee0561054ee596fbDa7791b1",
-
-	// Sonemium
-	"EVM-1946": "",
-	"EVM-1868": "0x7F0165140D0f3251c8f6465e94E9d12C7FD40711",
-}
+// Replaced by auto-generated version
 
 export interface ITokenPriceFeedDetails {
 	name: SupportedAssets

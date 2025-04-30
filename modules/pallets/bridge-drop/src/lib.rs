5b1f47aa24f516170121ddf2b717ed93f53ac1c2
David Salami <wizdave97@gmail.com>
2025-04-30 16:42:40 +0000

    complete airdrop pallet

--- modules/pallets/bridge-drop/src/lib.rs
+++ modules/pallets/bridge-drop/src/lib.rs
@@ -20,16 +20,31 @@ extern crate alloc;
 pub use pallet::*;
 use polkadot_sdk::*;
 
+/// Eighteen months in hyperbridge blocks
+const EIGHTEEN_MONTHS: u64 = 3_888_000;
+
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
-	use alloc::{vec, vec::Vec};
+	use alloc::{format, vec, vec::Vec};
+	use anyhow::anyhow;
+	use codec::Encode;
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
 	use polkadot_sdk::{
+		frame_support::{
+			traits::{Currency, ExistenceRequirement, VestingSchedule},
+			PalletId,
+		},
 		frame_system::ensure_none,
 		sp_core::{H160, H256},
+		sp_runtime::{traits::AccountIdConversion, Permill},
 	};
+	use rs_merkle::MerkleProof;
+
+	type VestingBalanceOf<T> = <<T as pallet_vesting::Config>::Currency as Currency<
+		<T as frame_system::Config>::AccountId,
+	>>::Balance;
 
 	#[pallet::pallet]
 	#[pallet::without_storage_info]
@@ -38,11 +53,17 @@ pub mod pallet {
 	/// The config trait
 	#[pallet::config]
 	pub trait Config:
-		polkadot_sdk::frame_system::Config + polkadot_sdk::pallet_balances::Config
+		polkadot_sdk::frame_system::Config
+		+ polkadot_sdk::pallet_balances::Config
+		+ pallet_ismp::Config
+		+ pallet_vesting::Config
 	{
 		/// The overarching event type.
 		type RuntimeEvent: From<Event<Self>>
 			+ IsType<<Self as polkadot_sdk::frame_system::Config>::RuntimeEvent>;
+
+		/// Currency implementation
+		type Currency: Currency<Self::AccountId>;
 	}
 
 	/// Set of accounts that have claimed the airdrop
@@ -50,59 +71,141 @@ pub mod pallet {
 	#[pallet::getter(fn claimed)]
 	pub type Claimed<T: Config> = StorageMap<_, Blake2_128Concat, H160, T::AccountId, OptionQuery>;
 
-	/// Merkle root
+	/// Merkle root and total leafcount
 	#[pallet::storage]
 	#[pallet::getter(fn merkle_root)]
-	pub type MerkleRoot<T: Config> = StorageValue<_, H256, OptionQuery>;
+	pub type MerkleRoot<T: Config> = StorageValue<_, (H256, u64), OptionQuery>;
+
+	/// Merkle root and total leafcount
+	#[pallet::storage]
+	#[pallet::getter(fn starting_block)]
+	pub type StartingBlock<T: Config> = StorageValue<_, BlockNumberFor<T>, OptionQuery>;
 
 	#[pallet::error]
 	pub enum Error<T> {
 		/// Account has claimed the airdrop already
 		AlreadyClaimed,
 		/// Invalid claim merkle proof
-		InvalidMerkleProof,
+		InvalidProof,
+		/// Pallet has not been initialized
+		MerkleRootNotFound,
 	}
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
 		/// Airdrop claimed
-		Claimed { account: T::AccountId, amount: <T as pallet_balances::Config>::Balance },
+		Claimed {
+			beneficiary: T::AccountId,
+			amount: <<T as Config>::Currency as Currency<T::AccountId>>::Balance,
+		},
 	}
 
 	#[derive(
 		Clone, codec::Encode, codec::Decode, scale_info::TypeInfo, PartialEq, Eq, RuntimeDebug,
 	)]
 	#[scale_info(skip_type_params(T))]
-	pub struct Proof<T: Config> {
+	pub struct Proof<AccountId, Balance> {
 		/// Account Eligible for the claim
 		who: H160,
 		/// Receiving account on Hyperbridge
-		dest: T::AccountId,
-		/// Signature that validates the receiving address
+		beneficiary: AccountId,
+		/// Signature that approves the receiving address
 		signature: Vec<u8>,
 		/// Merkle proof of eligibility
-		proof: Vec<H256>,
+		proof_items: Vec<H256>,
+		/// Leaf index for (who, amount) in the merkle tree
+		leaf_index: u64,
 		/// Amount to claim
-		amount: <T as pallet_balances::Config>::Balance,
+		amount: Balance,
 	}
 
-	struct Claim<T: Config> {
-		account: T::AccountId,
-		amount: u128,
+	#[derive(Clone, Copy)]
+	struct KeccakHasher;
+
+	impl rs_merkle::Hasher for KeccakHasher {
+		type Hash = [u8; 32];
+
+		fn hash(data: &[u8]) -> Self::Hash {
+			sp_io::hashing::keccak_256(data)
+		}
+	}
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+		fn on_initialize(n: BlockNumberFor<T>) -> Weight {
+			if StartingBlock::<T>::get().is_none() {
+				StartingBlock::<T>::put(n);
+				return <T as frame_system::Config>::DbWeight::get().reads_writes(1, 0);
+			}
+
+			<T as frame_system::Config>::DbWeight::get().reads_writes(1, 1)
+		}
 	}
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T>
 	where
-		<T as pallet_balances::Config>::Balance: From<u128>,
-		<T as frame_system::Config>::Hash: From<H256>,
+		<<T as Config>::Currency as Currency<T::AccountId>>::Balance: From<u128>,
+		u128: From<<<T as Config>::Currency as Currency<T::AccountId>>::Balance>,
+		VestingBalanceOf<T>: From<u128>,
 	{
-		/// Claim bridge tokens
+		/// Set merkle root for claims
 		#[pallet::call_index(0)]
 		#[pallet::weight(<T as frame_system::Config>::DbWeight::get().reads_writes(1, 2))]
-		pub fn claim_tokens(origin: OriginFor<T>, proof: Proof<T>) -> DispatchResult {
+		pub fn set_merkle_root(
+			origin: OriginFor<T>,
+			root: H256,
+			leaf_count: u64,
+		) -> DispatchResult {
+			T::AdminOrigin::ensure_origin(origin)?;
+
+			MerkleRoot::<T>::put((root, leaf_count));
+			Ok(())
+		}
+
+		/// Claim bridge tokens
+		#[pallet::call_index(1)]
+		#[pallet::weight(<T as frame_system::Config>::DbWeight::get().reads_writes(1, 2))]
+		pub fn claim_tokens(
+			origin: OriginFor<T>,
+			params: Proof<
+				T::AccountId,
+				<<T as Config>::Currency as Currency<T::AccountId>>::Balance,
+			>,
+		) -> DispatchResult {
 			ensure_none(origin)?;
+
+			let beneficiary = params.beneficiary.clone();
+			let amount = params.amount;
+			Self::execute_claim(params)?;
+
+			// Unlock 25% of token amount
+			let percent = Permill::from_parts(250_000);
+			let free_balance = percent * u128::from(amount);
+
+			let locked = u128::from(amount).saturating_sub(free_balance);
+
+			<<T as Config>::Currency as Currency<T::AccountId>>::transfer(
+				&Self::account_id(),
+				&beneficiary,
+				amount.into(),
+				ExistenceRequirement::AllowDeath,
+			)?;
+
+			let unlock_per_block = locked / EIGHTEEN_MONTHS as u128;
+
+			let starting_block =
+				StartingBlock::<T>::get().unwrap_or(frame_system::Pallet::<T>::block_number());
+
+			pallet_vesting::Pallet::<T>::add_vesting_schedule(
+				&beneficiary,
+				locked.into(),
+				unlock_per_block.into(),
+				starting_block,
+			)?;
+
+			Self::deposit_event(Event::<T>::Claimed { beneficiary, amount });
 
 			Ok(())
 		}
@@ -111,8 +214,9 @@ pub mod pallet {
 	#[pallet::validate_unsigned]
 	impl<T: Config> ValidateUnsigned for Pallet<T>
 	where
-		<T as frame_system::Config>::Hash: From<H256>,
-		<T as pallet_balances::Config>::Balance: From<u128>,
+		<<T as Config>::Currency as Currency<T::AccountId>>::Balance: From<u128>,
+		u128: From<<<T as Config>::Currency as Currency<T::AccountId>>::Balance>,
+		VestingBalanceOf<T>: From<u128>,
 	{
 		type Call = Call<T>;
 
@@ -121,15 +225,22 @@ pub mod pallet {
 			Ok(())
 		}
 
-		fn validate_unsigned(source: TransactionSource, call: &Self::Call) -> TransactionValidity {
-			let Call::claim_tokens { proof } = call else {
+		fn validate_unsigned(_source: TransactionSource, call: &Self::Call) -> TransactionValidity {
+			let Call::claim_tokens { params } = call else {
 				return Err(TransactionValidityError::Invalid(InvalidTransaction::Call));
+			};
+
+			let msg_hash = match Self::execute_claim(params.clone()) {
+				Ok(_) => sp_io::hashing::keccak_256(&params.encode()),
+				Err(_) => {
+					return Err(TransactionValidityError::Invalid(InvalidTransaction::Call));
+				},
 			};
 
 			Ok(ValidTransaction {
 				priority: 100,
 				requires: vec![],
-				provides: vec![],
+				provides: vec![msg_hash.to_vec()],
 				longevity: 25,
 				propagate: true,
 			})
@@ -137,8 +248,79 @@ pub mod pallet {
 	}
 
 	impl<T: Config> Pallet<T> {
-		fn validate_proof(proof: Proof<T>) -> Result<(), Error<T>> {
+		fn execute_claim(
+			params: Proof<
+				T::AccountId,
+				<<T as Config>::Currency as Currency<T::AccountId>>::Balance,
+			>,
+		) -> DispatchResult {
+			let (root, leaf_count) =
+				MerkleRoot::<T>::get().ok_or_else(|| Error::<T>::MerkleRootNotFound)?;
+
+			if Claimed::<T>::get(params.who.clone()).is_some() {
+				Err(Error::<T>::AlreadyClaimed)?
+			}
+
+			verify_proof(root, leaf_count, params.clone()).map_err(|_| Error::<T>::InvalidProof)?;
+
+			Claimed::<T>::insert(params.who, params.beneficiary);
 			Ok(())
 		}
+
+		/// Account that should hold all tokens for airdrop
+		pub fn account_id() -> T::AccountId {
+			PalletId(*b"BRIDGE//").into_account_truncating()
+		}
+	}
+
+	const ETHEREUM_MESSAGE_PREFIX: &'static str = "\x19Ethereum Signed Message:\n";
+	fn verify_proof<AccountId: Encode, Balance: Encode>(
+		merkle_root: H256,
+		leaf_count: u64,
+		params: Proof<AccountId, Balance>,
+	) -> Result<(), anyhow::Error> {
+		// Verify signature
+
+		if params.signature.len() != 65 {
+			Err(anyhow!("Invalid Signature"))?
+		}
+
+		let mut signature = [0u8; 65];
+
+		let payload = params.beneficiary.encode();
+
+		signature.copy_from_slice(&params.signature);
+
+		// Following EIP-191 convention https://eips.ethereum.org/EIPS/eip-191
+		let preimage = vec![
+			format!("{ETHEREUM_MESSAGE_PREFIX}{}", payload.len()).as_bytes().to_vec(),
+			payload,
+		]
+		.concat();
+		let message = sp_io::hashing::keccak_256(&preimage);
+		let pub_key = sp_io::crypto::secp256k1_ecdsa_recover(&signature, &message)
+			.map_err(|_| anyhow!("Failed to recover ecdsa public key from signature"))?;
+		let eth_address = H160::from_slice(&sp_io::hashing::keccak_256(&pub_key[..])[12..]);
+
+		if eth_address != params.who {
+			Err(anyhow!("Invalid Signature"))?
+		}
+
+		let proof = MerkleProof::<KeccakHasher>::new(
+			params.proof_items.into_iter().map(|val| val.0).collect(),
+		);
+
+		let leaf_hash = sp_io::hashing::keccak_256(&(params.who, params.amount).encode());
+
+		if !proof.verify(
+			merkle_root.0,
+			&[params.leaf_index as usize],
+			&[leaf_hash],
+			leaf_count as usize,
+		) {
+			Err(anyhow!("Invalid Merkle Proof"))?
+		}
+
+		Ok(())
 	}
 }

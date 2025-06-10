24d4a5429167ee42a1e0a4ef41ae3b5ce35ce72e
dharjeezy <dharjeezy@gmail.com>
2025-06-10 16:02:02 +0100

    nit

--- modules/pallets/ismp/src/lib.rs
+++ modules/pallets/ismp/src/lib.rs
@@ -50,12 +50,14 @@ pub mod pallet {
 	use codec::{Codec, Encode};
 	use core::fmt::Debug;
 	use frame_support::{
-		dispatch::{DispatchResult, DispatchResultWithPostInfo},
+		dispatch::DispatchResult,
 		pallet_prelude::*,
 		traits::{fungible::Mutate, tokens::Preservation, Get, UnixTime},
 		PalletId,
 	};
 	use frame_system::pallet_prelude::{BlockNumberFor, *};
+	#[cfg(feature = "unsigned")]
+	use ismp::messaging::Message;
 	use ismp::{
 		consensus::{
 			ConsensusClientId, ConsensusStateId, StateCommitment, StateMachineHeight,
@@ -64,7 +66,7 @@ pub mod pallet {
 		events::{RequestResponseHandled, TimeoutHandled},
 		handlers,
 		host::{IsmpHost, StateMachine},
-		messaging::{CreateConsensusState, Message},
+		messaging::CreateConsensusState,
 		router::IsmpRouter,
 	};
 	use sp_core::{storage::ChildInfo, H256};
@@ -380,8 +382,9 @@ pub mod pallet {
 
 			let metadata = match message.commitment {
 				MessageCommitment::Request(commitment) => RequestCommitments::<T>::get(commitment),
-				MessageCommitment::Response(commitment) =>
-					ResponseCommitments::<T>::get(commitment),
+				MessageCommitment::Response(commitment) => {
+					ResponseCommitments::<T>::get(commitment)
+				},
 			};
 
 			let Some(mut metadata) = metadata else {
@@ -531,10 +534,11 @@ pub mod pallet {
 				// check that requests will be successfully dispatched
 				// so we can not be spammed with failing txs
 				.map(|result| match result {
-					MessageResult::Request(results) |
-					MessageResult::Response(results) |
-					MessageResult::Timeout(results) =>
-						results.into_iter().map(|result| result.map(|_| ())).collect::<Vec<_>>(),
+					MessageResult::Request(results)
+					| MessageResult::Response(results)
+					| MessageResult::Timeout(results) => {
+						results.into_iter().map(|result| result.map(|_| ())).collect::<Vec<_>>()
+					},
 					MessageResult::ConsensusMessage(_) | MessageResult::FrozenClient(_) => {
 						vec![Ok(())]
 					},

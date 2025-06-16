8d9c54c3a1cb833999d900a0f3e006cfbab5a79e
dharjeezy <dharjeezy@gmail.com>
2025-06-16 16:32:26 +0100

    include arbitrum consensus clients

---
+++ modules/ismp/clients/ismp-arbitrum/src/lib.rs
@@ -0,0 +1,206 @@
+// Copyright (C) Polytope Labs Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! ISMP Consensus Client for the Arbitrum Consensus Protocol.
+#![cfg_attr(not(feature = "std"), no_std)]
+#![allow(unused_variables)]
+
+extern crate alloc;
+
+pub mod pallet;
+use arbitrum_verifier::{
+	ArbitrumBoldProof, ArbitrumPayloadProof, verify_arbitrum_bold, verify_arbitrum_payload,
+};
+use pallet::Pallet;
+use pallet::SupportedStateMachines;
+
+use crate::pallet::Config;
+use alloc::{collections::BTreeMap, string::ToString};
+use codec::Decode;
+use codec::Encode;
+use evm_state_machine::EvmStateMachine;
+use ismp::{
+	consensus::{
+		ConsensusClient, ConsensusClientId, ConsensusStateId, StateMachineClient,
+		StateMachineHeight, StateMachineId, VerifiedCommitments,
+	},
+	error::Error,
+	host::{IsmpHost, StateMachine},
+	messaging::StateCommitmentHeight,
+};
+use primitive_types::H256;
+
+pub const ARBITRUM_CONSENSUS_CLIENT_ID: ConsensusClientId = *b"ARBC";
+
+#[derive(codec::Encode, codec::Decode, Debug, PartialEq, Eq, Clone)]
+pub struct ConsensusState {
+	pub finalized_height: u64,
+	pub state_machine_id: StateMachineId,
+	pub l1_state_machine_id: StateMachineId,
+	pub state_root: H256,
+}
+
+#[derive(Encode, Decode)]
+pub struct ArbitrumUpdate {
+	pub state_machine_id: StateMachineId,
+	pub l1_height: u64,
+	pub proof: ArbitrumConsensusProof,
+}
+
+/// Description of the various consensus mechanics supported for Arbitrum
+#[derive(Encode, Decode, Debug)]
+pub enum ArbitrumConsensusProof {
+	ArbitrumOrbit(ArbitrumPayloadProof),
+	ArbitrumBold(ArbitrumBoldProof),
+}
+
+pub struct ArbitrumConsensusClient<
+	H: IsmpHost,
+	C: Config,
+	T: pallet_ismp_host_executive::Config + crate::pallet::Config,
+>(core::marker::PhantomData<(H, C, T)>);
+
+impl<
+	H: IsmpHost + Send + Sync + Default + 'static,
+	C: Config + Send + Sync + Default + 'static,
+	T: pallet_ismp_host_executive::Config + pallet::Config + 'static,
+> Default for ArbitrumConsensusClient<H, C, T>
+{
+	fn default() -> Self {
+		Self(core::marker::PhantomData)
+	}
+}
+
+impl<
+	H: IsmpHost + Send + Sync + Default + 'static,
+	C: Config + Send + Sync + Default + 'static,
+	T: pallet_ismp_host_executive::Config + pallet::Config + 'static,
+> Clone for ArbitrumConsensusClient<H, C, T>
+{
+	fn clone(&self) -> Self {
+		Self(core::marker::PhantomData)
+	}
+}
+
+impl<
+	H: IsmpHost + Send + Sync + Default + 'static,
+	C: Config + Send + Sync + Default + 'static,
+	T: pallet_ismp_host_executive::Config + pallet::Config + 'static,
+> ConsensusClient for ArbitrumConsensusClient<H, C, T>
+{
+	fn verify_consensus(
+		&self,
+		host: &dyn IsmpHost,
+		consensus_state_id: ConsensusStateId,
+		trusted_consensus_state: Vec<u8>,
+		consensus_proof: Vec<u8>,
+	) -> Result<(Vec<u8>, VerifiedCommitments), Error> {
+		let ArbitrumUpdate { state_machine_id, l1_height, proof } =
+			ArbitrumUpdate::decode(&mut &consensus_proof[..])
+				.map_err(|_| Error::Custom("Cannot decode arbitrum update".to_string()))?;
+
+		let mut consensus_state = ConsensusState::decode(&mut &trusted_consensus_state[..])
+			.map_err(|_| Error::Custom("Cannot decode trusted consensus state".to_string()))?;
+
+		let l1_state_machine_height =
+			StateMachineHeight { id: consensus_state.l1_state_machine_id, height: l1_height };
+
+		let l1_state_commitment = host.state_machine_commitment(l1_state_machine_height)?;
+		let state_root = l1_state_commitment.state_root;
+
+		let mut state_machine_map: BTreeMap<StateMachine, Vec<StateCommitmentHeight>> =
+			BTreeMap::new();
+
+		match proof {
+			ArbitrumConsensusProof::ArbitrumOrbit(proof) => {
+				if let Some(rollup_core_address) =
+					Pallet::<T>::state_machines_rollup_core_addresses(state_machine_id)
+				{
+					let state = verify_arbitrum_payload::<H>(
+						proof,
+						state_root,
+						rollup_core_address,
+						consensus_state_id.clone(),
+					)?;
+
+					let state_commitment_height = StateCommitmentHeight {
+						commitment: state.commitment,
+						height: state.height.height,
+					};
+
+					let mut state_commitment_vec: Vec<StateCommitmentHeight> = Vec::new();
+					state_commitment_vec.push(state_commitment_height);
+					state_machine_map
+						.insert(consensus_state.state_machine_id.state_id, state_commitment_vec);
+
+					consensus_state.finalized_height = state.height.height;
+					consensus_state.state_root = state.commitment.state_root;
+				}
+			},
+			ArbitrumConsensusProof::ArbitrumBold(proof) => {
+				if let Some(rollup_core_address) =
+					Pallet::<T>::state_machines_rollup_core_addresses(state_machine_id)
+				{
+					let state = verify_arbitrum_bold::<H>(
+						proof,
+						state_root,
+						rollup_core_address,
+						consensus_state_id.clone(),
+					)
+					.map_err(|error| {
+						Error::Custom(format!("Error verifying arbitrum payload {:?}", &error))
+					})?;
+
+					let state_commitment_height = StateCommitmentHeight {
+						commitment: state.commitment,
+						height: state.height.height,
+					};
+
+					let mut state_commitment_vec: Vec<StateCommitmentHeight> = Vec::new();
+					state_commitment_vec.push(state_commitment_height);
+					state_machine_map
+						.insert(consensus_state.state_machine_id.state_id, state_commitment_vec);
+
+					consensus_state.finalized_height = state.height.height;
+					consensus_state.state_root = state.commitment.state_root;
+				}
+			},
+		}
+
+		Ok((consensus_state.encode(), state_machine_map))
+	}
+
+	fn verify_fraud_proof(
+		&self,
+		_host: &dyn IsmpHost,
+		_trusted_consensus_state: Vec<u8>,
+		_proof_1: Vec<u8>,
+		_proof_2: Vec<u8>,
+	) -> Result<(), Error> {
+		Err(Error::Custom("fraud proof verification unimplemented".to_string()))
+	}
+
+	fn consensus_client_id(&self) -> ConsensusClientId {
+		ARBITRUM_CONSENSUS_CLIENT_ID
+	}
+
+	fn state_machine(&self, id: StateMachine) -> Result<Box<dyn StateMachineClient>, Error> {
+		if SupportedStateMachines::<T>::contains_key(id) {
+			Ok(Box::new(<EvmStateMachine<H, T>>::default()))
+		} else {
+			Err(Error::Custom("State machine not supported".to_string()))
+		}
+	}
+}
---
+++ modules/ismp/clients/ismp-arbitrum/src/pallet.rs
@@ -0,0 +1,100 @@
+// Copyright (C) Polytope Labs Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+pub use pallet::*;
+use polkadot_sdk::*;
+
+extern crate alloc;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use super::*;
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+	use ismp::host::StateMachine;
+	use ismp::{consensus::StateMachineId, host::IsmpHost};
+	use primitive_types::H160;
+
+	#[pallet::pallet]
+	#[pallet::without_storage_info]
+	pub struct Pallet<T>(_);
+
+	/// The config trait
+	#[pallet::config]
+	pub trait Config: polkadot_sdk::frame_system::Config + pallet_ismp::Config {
+		/// The overarching event type
+		type RuntimeEvent: From<Event<Self>>
+			+ IsType<<Self as polkadot_sdk::frame_system::Config>::RuntimeEvent>;
+
+		/// Origin allowed for admin privileges
+		type AdminOrigin: EnsureOrigin<<Self as frame_system::Config>::RuntimeOrigin>;
+		/// IsmpHost implementation
+		type IsmpHost: IsmpHost + Default;
+	}
+
+	#[pallet::error]
+	pub enum Error<T> {}
+
+	// Mapping from state machineId to respective roll up core addresses
+	#[pallet::storage]
+	#[pallet::getter(fn state_machines_rollup_core_addresses)]
+	pub type StateMachinesRollupCoreAddresses<T: Config> =
+		StorageMap<_, Blake2_128Concat, StateMachineId, H160, OptionQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn supported_state_machines)]
+	pub type SupportedStateMachines<T: Config> =
+		StorageMap<_, Twox64Concat, StateMachine, bool, OptionQuery>;
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		/// State Machine Rollup Core Address updated
+		StateMachinesRollupCoreAddress {
+			state_machine_id: StateMachineId,
+			rollup_core_address: H160,
+		},
+	}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		/// Sets the new roll up core address
+		#[pallet::call_index(0)]
+		#[pallet::weight(<T as frame_system::Config>::DbWeight::get().reads_writes(1, 1))]
+		pub fn set_rollup_core_address(
+			origin: OriginFor<T>,
+			state_machine_id: StateMachineId,
+			rollup_core_address: H160,
+		) -> DispatchResult {
+			<T as Config>::AdminOrigin::ensure_origin(origin)?;
+
+			StateMachinesRollupCoreAddresses::<T>::mutate(
+				state_machine_id.clone(),
+				|maybe_address| {
+					*maybe_address = Some(rollup_core_address);
+				},
+			);
+
+			SupportedStateMachines::<T>::insert(state_machine_id.state_id, true);
+
+			Self::deposit_event(Event::<T>::StateMachinesRollupCoreAddress {
+				state_machine_id,
+				rollup_core_address,
+			});
+
+			Ok(())
+		}
+	}
+}

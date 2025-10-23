a64c474c34d9504a14fd8f7f3c0940e79c6d809a
David Salami <wizdave97@gmail.com>
2025-10-23 09:21:50 +0000

    bump runtime version

--- parachain/runtimes/gargantua/src/lib.rs
+++ parachain/runtimes/gargantua/src/lib.rs
@@ -239,7 +239,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("gargantua"),
 	impl_name: Cow::Borrowed("gargantua"),
 	authoring_version: 1,
-	spec_version: 4_400,
+	spec_version: 4_500,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,

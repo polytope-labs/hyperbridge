ba8cdaee56bd819fa285ff52a9c36aac8d7f27d0
Seun Lanlege <seun@polytope.technology>
2025-10-27 15:06:12 +0000

    redeploy intent gateway

--- evm/script/deploy.sh
+++ evm/script/deploy.sh
@@ -7,5 +7,6 @@ source "$(pwd)/.env.$3"
 rm -rf out/ cache/ broadcast/
 # deploy
 HOST=$1 forge script "script/Deploy$2.s.sol:DeployScript" --rpc-url "${1,,}" -vvv --sender="$ADMIN" --broadcast
+# HOST=$1 forge script "script/Deploy$2.s.sol:DeployScript" --rpc-url "${1,,}" -vvv --sender="$ADMIN" 
 # verify
 HOST=$1 forge script "script/Deploy$2.s.sol:DeployScript" --rpc-url "${1,,}" --resume --verify -vvvvv --private-keys $PRIVATE_KEY

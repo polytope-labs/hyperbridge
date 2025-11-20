9ce386ead023eb94f4ada33a3d93a310d92f09d5
dharjeezy <dharjeezy@gmail.com>
2025-11-20 21:51:21 +0100

    nit

--- modules/pallets/token-gateway/src/lib.rs
+++ modules/pallets/token-gateway/src/lib.rs
@@ -1,4 +1,4 @@
-	// Copyright (C) Polytope Labs Ltd.
+// Copyright (C) Polytope Labs Ltd.
 // SPDX-License-Identifier: Apache-2.0
 
 // Licensed under the Apache License, Version 2.0 (the "License");

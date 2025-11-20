27c11d95adee9569974539b8384300c61300d56c
David Salami <wizdave97@gmail.com>
2025-11-20 23:54:52 +0000

    allow removing consensus incentives

--- modules/pallets/ismp/src/host.rs
+++ modules/pallets/ismp/src/host.rs
@@ -201,7 +201,10 @@ impl<T: Config> IsmpHost for Pallet<T> {
 		if let Some(latest) = LatestStateMachineHeight::<T>::get(height.id) {
 			if latest == height.height {
 				// Reset back to the initial height to allow for honest updates
-				LatestStateMachineHeight::<T>::insert(height.id, 1);
+				LatestStateMachineHeight::<T>::insert(
+					height.id,
+					PreviousStateMachineHeight::<T>::get(height.id),
+				);
 			}
 		}
 		Ok(())

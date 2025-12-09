008bdf48995873ebd45248977cca2e3f5329af28
royvardhan <royvardhan.official@gmail.com>
2025-12-09 17:22:05 +0530

    fix script build

--- evm/script/DeployUniV4Wrapper.sol
+++ evm/script/DeployUniV4Wrapper.sol
@@ -15,8 +15,8 @@ contract DeployScript is BaseScript {
 
         address universalRouter = config.get("UNIVERSAL_ROUTER").toAddress();
         address quoter = config.get("V4_QUOTER").toAddress();
-        uint24 defaultFee = uint24(config.getUint("DEFAULT_FEE"));
-        int24 defaultTickSpacing = int24(int256(config.getUint("DEFAULT_TICK_SPACING")));
+        uint24 defaultFee = uint24(config.get("DEFAULT_FEE").toUint256());
+        int24 defaultTickSpacing = int24(int256(config.get("DEFAULT_TICK_SPACING").toUint256()));
 
         UniV4UniswapV2Wrapper wrapper = new UniV4UniswapV2Wrapper{salt: salt}(admin);
         wrapper.init(UniV4UniswapV2Wrapper.Params({

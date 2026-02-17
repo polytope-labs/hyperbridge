26971f8098bfde0d2b324f7b2184e513f8cdd1d0
David Salami <wizdave97@gmail.com>
2026-02-17 14:25:53 +0000

    patch polkadot-sdk

--- parachain/runtimes/nexus/src/lib.rs
+++ parachain/runtimes/nexus/src/lib.rs
@@ -227,7 +227,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("nexus"),
 	impl_name: Cow::Borrowed("nexus"),
 	authoring_version: 1,
-	spec_version: 6_300,
+	spec_version: 6_400,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,

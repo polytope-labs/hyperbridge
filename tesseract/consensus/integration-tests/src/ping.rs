75e4aeccb7aa0fe0b64d23e2f08a39c4f43d4882
David Salami <wizdave97@gmail.com>
2026-02-23 18:36:54 +0000

    nit

--- tesseract/consensus/integration-tests/src/ping.rs
+++ tesseract/consensus/integration-tests/src/ping.rs
@@ -32,21 +32,22 @@ const PING_ADDR: Address = Address::new(hex!("FE9f23F0F2fE83b8B9576d3FC94e9a7458
 #[ignore]
 async fn dispatch_ping() -> anyhow::Result<()> {
 	dotenv::dotenv().ok();
-	let _op_url = std::env::var("OP_URL").expect("OP_URL was missing in env variables");
-	let _base_url = std::env::var("BASE_URL").expect("BASE_URL was missing in env variables");
-	let _arb_url = std::env::var("ARB_URL").expect("ARB_URL was missing in env variables");
-	let _geth_url = std::env::var("GETH_URL").expect("GETH_URL was missing in env variables");
+	// let _polygon_url = std::env::var("POLYGON_URL").expect("OP_URL was missing in env variables");
+	let _arb_url = std::env::var("ARBITRUM_URL").expect("ARB_URL was missing in env variables");
+	let _sepolia_url = std::env::var("SEPOLIA_URL").expect("GETH_URL was missing in env variables");
 	let _bsc_url = std::env::var("BSC_URL").expect("BSC_URL was missing in env variables");
 	let respond = option_env!("RESPOND");
+
+	println!("{_arb_url}\n{_sepolia_url}\n{_bsc_url}");
 
 	let signing_key =
 		std::env::var("SIGNING_KEY").expect("SIGNING_KEY was missing in env variables");
 
 	let chains = vec![
-		(StateMachine::Evm(11155111), _geth_url, 6328728),
+		(StateMachine::Evm(11155111), _sepolia_url, 6328728),
 		(StateMachine::Evm(421614), _arb_url, 64565289),
-		(StateMachine::Evm(11155420), _op_url, 14717202),
-		(StateMachine::Evm(84532), _base_url, 10218678),
+		// (StateMachine::Evm(11155420), _op_url, 14717202),
+		// (StateMachine::Evm(84532), _base_url, 10218678),
 		(StateMachine::Evm(97), _bsc_url, 42173080),
 	];
 
@@ -55,7 +56,7 @@ async fn dispatch_ping() -> anyhow::Result<()> {
 		state_machine: StateMachine::Kusama(2000),
 		max_rpc_payload_size: None,
 		hashing: Some(HashAlgorithm::Keccak),
-		consensus_state_id: Some("PARA".to_string()),
+		consensus_state_id: Some("PAS0".to_string()),
 		// rpc_ws: "wss://hyperbridge-paseo-rpc.blockops.network:443".to_string(),
 		rpc_ws: "ws://127.0.0.1:9001".to_string(),
 		signer: format!("{:?}", H256::random()),
@@ -96,7 +97,7 @@ async fn dispatch_ping() -> anyhow::Result<()> {
 						rpc_urls: vec![url.clone()],
 						ismp_host: H160::from_slice(host_addr.as_slice()),
 						state_machine: chain.clone(),
-						consensus_state_id: "PARA".to_string(),
+						consensus_state_id: "PAS0".to_string(),
 						signer: signing_key.clone(),
 						tracing_batch_size: Default::default(),
 						query_batch_size: Default::default(),
@@ -195,7 +196,7 @@ async fn dispatch_ping() -> anyhow::Result<()> {
 								module: PING_ADDR,
 								timeout: 10 * 60 * 60,
 								fee: AlloyU256::from(30_000_000_000_000_000_000u128),
-								count: AlloyU256::from(100),
+								count: AlloyU256::from(50),
 							});
 							let gas = call
 								.estimate_gas()

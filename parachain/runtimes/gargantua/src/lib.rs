4821ae3ffdd0c869e5f62a11d1f43afa21e4ee1e
David Salami <wizdave97@gmail.com>
2026-03-17 15:09:24 +0000

    update signature threshold in bsc consensus client

--- parachain/runtimes/gargantua/src/lib.rs
+++ parachain/runtimes/gargantua/src/lib.rs
@@ -238,7 +238,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("gargantua"),
 	impl_name: Cow::Borrowed("gargantua"),
 	authoring_version: 1,
-	spec_version: 5_000,
+	spec_version: 5_100,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,

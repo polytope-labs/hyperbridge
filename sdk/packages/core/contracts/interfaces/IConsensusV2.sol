491c410a439ff20c1a89026aa6f2933bf4a76f6b
dharjeezy <dharjeezy@gmail.com>
2026-03-26 10:39:15 +0100

    [evm] Introduce HandlerV2 with batch call support (#723)

---
+++ sdk/packages/core/contracts/interfaces/IConsensusV2.sol
@@ -0,0 +1,39 @@
+// Copyright (C) Polytope Labs Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+pragma solidity ^0.8.17;
+
+import {IntermediateState} from "./IConsensus.sol";
+
+/**
+ * @title IConsensusV2
+ * @author Polytope Labs (hello@polytope.technology)
+ * @notice V2 consensus verification interface that additionally returns epoch/authority-set information.
+ * @dev Consensus clients implement this interface to verify state transitions and report authority set changes.
+ */
+interface IConsensusV2 {
+    /**
+     * @dev Verifies a consensus proof and returns the updated state, newly finalized intermediate states,
+     * and the next authority set ID.
+     * @param previousState The current trusted consensus state (encoded based on consensus type)
+     * @param proof The consensus proof to be verified
+     * @return The new consensus state after verification (encoded)
+     * @return Array of newly finalized intermediate states that can be trusted
+     * @return The next authority set ID
+     */
+    function verify(bytes calldata previousState, bytes calldata proof)
+        external
+        view
+        returns (bytes memory, IntermediateState[] memory, uint256);
+}

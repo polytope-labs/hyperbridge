62f6f1ea9f5c8abdc6eb305cb185de27fc166652
dharjeezy <dharjeezy@gmail.com>
2026-03-31 13:40:42 +0100

    remove indexer approach

--- tesseract/consensus/relayer/bins/prover.rs
+++ tesseract/consensus/relayer/bins/prover.rs
@@ -57,16 +57,6 @@ async fn main() -> Result<(), anyhow::Error> {
 		SubstrateClient::new(substrate_config.try_into()?).await?
 	};
 
-	let proof_indexer = match config.remove("indexer_db_url") {
-		Some(val) => {
-			let db_url: String = val.try_into()?;
-			let indexer = proof_indexer::ProofIndexer::initialize(&db_url).await?;
-			log::info!("ZK proof indexer connected to indexer DB");
-			Some(Arc::new(indexer))
-		},
-		None => None,
-	};
-
 	let mut beefy_prover = {
 		let beefy_config: tesseract_beefy::prover::BeefyProverConfig = config
 			.remove("beefy")
@@ -87,7 +77,7 @@ async fn main() -> Result<(), anyhow::Error> {
 			KeccakSubstrateChain,
 			zk_beefy::LocalProver,
 			tesseract_beefy::backend::RedisProofBackend,
-		>::new(beefy_config, substrate, prover, backend, proof_indexer)
+		>::new(beefy_config, substrate, prover, backend)
 		.await?
 	};
 

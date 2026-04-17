17f6caf30cdeb3f4bc49c8bf11a22ca6651164ea
Seun Lanlege <seun@polytope.technology>
2026-04-17 11:28:53 +0000

    feat: BEEFY consensus client with naive and SP1 proof paths

---
+++ modules/ismp/clients/beefy/src/lib.rs
@@ -0,0 +1,47 @@
+// Copyright (C) Polytope Labs Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+extern crate alloc;
+extern crate core;
+pub mod consensus;
+
+use polkadot_sdk::*;
+
+/// Crypto implementation using substrate host functions
+pub struct SubstrateCrypto;
+
+impl ismp::messaging::Keccak256 for SubstrateCrypto {
+	fn keccak256(bytes: &[u8]) -> primitive_types::H256 {
+		sp_io::hashing::keccak_256(bytes).into()
+	}
+}
+
+impl beefy_verifier::EcdsaRecover for SubstrateCrypto {
+	fn secp256k1_recover(prehash: &[u8; 32], signature: &[u8; 65]) -> anyhow::Result<[u8; 64]> {
+		sp_io::crypto::secp256k1_ecdsa_recover(signature, prehash)
+			.map_err(|_| anyhow::anyhow!("Failed to recover secp256k1 public key"))
+	}
+}
+
+/// Provides parachain tracking and SP1 vkey data to the BEEFY consensus client.
+pub trait BeefyClientConfig {
+	/// Returns true if the given parachain id is tracked by this consensus client.
+	fn is_parachain_tracked(para_id: u32) -> bool;
+
+	/// Returns the SP1 verification key hash bytes.
+	fn sp1_vkey_hash() -> alloc::vec::Vec<u8>;
+}

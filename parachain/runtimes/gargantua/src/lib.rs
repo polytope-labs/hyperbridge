cb6769dc3acdf178c3acf68f07da6bac92dd8c86
Seun Lanlege <seun@polytope.technology>
2026-04-23 23:46:41 +0000

    bump gargantua runtime

--- parachain/runtimes/gargantua/src/lib.rs
+++ parachain/runtimes/gargantua/src/lib.rs
@@ -248,7 +248,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("gargantua"),
 	impl_name: Cow::Borrowed("gargantua"),
 	authoring_version: 1,
-	spec_version: 5_900,
+	spec_version: 5_950,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,

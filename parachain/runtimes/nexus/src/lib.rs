1d58d0111eb8f5a99cb46f3ce466fc8805d62fe9
David Salami <wizdave97@gmail.com>
2026-05-20 14:41:56 +0000

    nexus version 7000

--- parachain/runtimes/nexus/src/lib.rs
+++ parachain/runtimes/nexus/src/lib.rs
@@ -235,7 +235,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("nexus"),
 	impl_name: Cow::Borrowed("nexus"),
 	authoring_version: 1,
-	spec_version: 6_900,
+	spec_version: 7_000,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,

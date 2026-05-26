bbe12960d9a1f9a44073e9e2b94ddefee14944f4
Seun Lanlege <seun@polytope.technology>
2026-05-26 03:53:38 +0000

    reset latest height to 1

--- parachain/runtimes/nexus/src/lib.rs
+++ parachain/runtimes/nexus/src/lib.rs
@@ -235,7 +235,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("nexus"),
 	impl_name: Cow::Borrowed("nexus"),
 	authoring_version: 1,
-	spec_version: 7_300,
+	spec_version: 7_400,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,

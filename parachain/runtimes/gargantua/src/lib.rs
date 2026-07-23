1fa5a5a69040bb48752188c4ee745dd7127625a9
dharjeezy <dharjeezy@gmail.com>
2026-07-23 11:11:59 +0100

    bump runtime and node versions

--- parachain/runtimes/gargantua/src/lib.rs
+++ parachain/runtimes/gargantua/src/lib.rs
@@ -251,7 +251,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("gargantua"),
 	impl_name: Cow::Borrowed("gargantua"),
 	authoring_version: 1,
-	spec_version: 7_900,
+	spec_version: 8_000,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
